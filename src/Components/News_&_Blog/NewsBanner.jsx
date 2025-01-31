43b4869f05889f54d8136ad18c9dd783170c43c9
karansapra124 <sapra7112@gmail.com>
2025-01-31 11:02:07 +0530

    Added Button To News Banner

--- src/Components/News_&_Blog/NewsBanner.jsx
+++ src/Components/News_&_Blog/NewsBanner.jsx
@@ -1,36 +1,45 @@
 import React from "react";
 import Container from "../Helper/Container";
 import Divider from "../Helper/Divider";
+import { FaPaperPlane } from "react-icons/fa";
 const NewsBanner = () => {
   return (
-  <div className="bg-gray-200/40 h-full">
-      <div className="bg-news-bg relative mx-auto  rounded-lg max-w-screen-xl  shadow-sm shadow-white  bg-cover bg-no-repeat">
-      <div className="absolute inset-0 h-full w-full rounded-lg bg-black/80 "></div>
-      <Container className={"relative brightness-110 "}>
-      <img src="call.gif" className="absolute h-10  -left-5 -top-5 rounded-full shadow-md shadow-yellow-600 w-10" alt="" srcset="" />
+    <div className="h-full bg-gray-200/40">
+      <div className="relative mx-auto max-w-screen-xl rounded-lg bg-news-bg bg-cover bg-no-repeat shadow-sm shadow-white">
+        <div className="absolute inset-0 h-full w-full rounded-lg bg-black/80"></div>
+        <Container className={"relative brightness-110"}>
+          <img
+            src="call.gif"
+            className="absolute -left-5 -top-5 h-10 w-10 rounded-full shadow-md shadow-yellow-600"
+            alt=""
+            srcset=""
+          />
 
-        <h1 className="text-3xl font-extrabold uppercase  text-yellow-600">
-          Get the desired <br />{" "}
-          <span className="text-primary-color  brightness-200">
-            news at one place
-          </span>
-        </h1>
-        <p className="text-xs font-semibold uppercase my-4 text-gray-300">
-          Get All News Updates At Your Device.
-        </p>
-        <Divider className={"my-4 h-1 w-12 rounded-full bg-yellow-600"} />
-        <input
-          type="text"
-          min={0}
-          autoFocus
-          name=""
-          id=""
-          placeholder="Enter The Number..."
-          className="w-96 border-l-2 border-b-2 rounded-md   border-yellow-600 text-primary-color font-extralight  bg-white/40 p-2 "
-        />
-      </Container>
+          <h1 className="text-3xl font-extrabold uppercase text-yellow-600">
+            Get the desired <br />{" "}
+            <span className="text-primary-color brightness-200">
+              news at one place
+            </span>
+          </h1>
+          <p className="my-4 text-xs font-semibold uppercase text-gray-300">
+            Get All News Updates At Your Device.
+          </p>
+          <Divider className={"my-4 h-1 w-12 rounded-full bg-yellow-600"} />
+          <div className="flex items-center gap-2">
+            <input
+              type="text"
+              min={0}
+              autoFocus
+              name=""
+              id=""
+              placeholder="Enter The Number..."
+              className="w-96 rounded-md border-b-2 border-l-2 border-yellow-600 bg-white/40 p-2 font-extralight text-primary-color"
+            />
+            <FaPaperPlane className="bg-yellow-600 p-4 rounded-md text-[3rem] cursor-pointer hover:scale-105 transition-all text-white"/>
+          </div>
+        </Container>
+      </div>
     </div>
-  </div>
   );
 };
 
