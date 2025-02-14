58a695c2aa402746c39885801dedf5018a218c72
karansapra124 <sapra7112@gmail.com>
2025-02-14 16:54:34 +0530

    Updated BookSlot of medical-expo

--- src/Components/Medical_Expo/BookSpot.jsx
+++ src/Components/Medical_Expo/BookSpot.jsx
@@ -4,14 +4,14 @@ const BookSpot = () => {
     return (
         <>
             <Container className={'bg-gradient-to-tr from-black to-black/85'} >
-                <div className='flex p-4 justify-between max-w-screen-lg mx-auto bg-black/20 border border-gray-700 rounded-md'>
+                <div className='flex p-4 justify-between max-w-screen-lg mx-auto bg-black/20 border border-gray-200 rounded-md'>
                     <div className='p-2'>
                         <h1 className='text-gray-300 font-bold text-xl pb-2'>
                             Book your slot now!
                         </h1>
                         <p className='text-gray-500 font-semibold text-xs max-w-lg '> Attend the Medical Mentor’s Expo, join the movement for medical education excellence, and shape your medical career</p>
                     </div>
-                    <button className='bg-yellow-600 w-fit h-fit text-white font-semibold text-sm my-auto px-2 py-1 rounded-l-full rounded-r-full'>Book Free Slot</button>
+                    <button className='bg-yellow-600 hover:bg-yellow-700 transition-all w-fit h-fit text-white font-semibold text-sm my-auto px-4 py-2 rounded-l-full rounded-r-full'>Book Free Slot</button>
                 </div>
             </Container>
         </>
