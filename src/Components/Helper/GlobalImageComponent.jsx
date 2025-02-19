764bfdabfd27765318f28e873f2213a303adacba
karansapra124 <sapra7112@gmail.com>
2025-02-19 12:06:33 +0530

    Gallery Improved

--- src/Components/Helper/GlobalImageComponent.jsx
+++ src/Components/Helper/GlobalImageComponent.jsx
@@ -8,33 +8,39 @@ const GlobalImageComponent = ({ image }) => {
   useEffect(() => console.log(mainImage), [mainImage]);
   return (
     <>
-      <div className="max-w-screen-md  mx-auto">
-        <div>
+      <div className="mx-auto ">
+        <div className="flex flex-row max-w-screen-xl">
           <img
             key={mainImage}
             src={image[mainImage].imageUrl}
-            className="animate-zoomIn  relative  max-w-2xl mx-auto "
+            className="w-full h-[30.5rem] mx-auto  animate-zoomIn"
             alt=""
             srcset=""
           />
           <Carousel
             dots={false}
+            vertical={true}
             afterChange={() =>
               setMainImage((prev) => (prev < image.length - 1 ? prev + 1 : 0))
             }
-            className="absolute max-w-screen-sm -top-20 left-36 "
-            slidesToShow={5}
-            
+            className="max-w-screen-sm "
+            slidesToShow={4}
             autoplaySpeed={3000}
-
             infinite
             autoplay
           >
             {image?.map((elem, index) => {
-              return <>
-              {/* <div key={index} className="absolute w-full h-full bg-black/10 z-[999]"></div> */}
-              <img key={index} className="shadow-sm shadow-black" src={elem.imageUrl} alt={elem} />
-              </>
+              return (
+                <>
+                  {/* <div key={index} className="absolute w-full h-full bg-black/10 z-[999]"></div> */}
+                  <img
+                    key={index}
+                    className="shadow-sm w-40 shadow-black"
+                    src={elem.imageUrl}
+                    alt={elem}
+                  />
+                </>
+              );
             })}
           </Carousel>
         </div>
