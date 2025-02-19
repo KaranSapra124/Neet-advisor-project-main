3a3c3b9d7559a33a5883e4c3fb8a7354625f01f6
karansapra124 <sapra7112@gmail.com>
2025-02-19 15:21:21 +0530

    Added responsiveness to coounselling Section Of Homepage

--- src/Components/Home/CounsellingSection.jsx
+++ src/Components/Home/CounsellingSection.jsx
@@ -6,9 +6,9 @@ import ScrollAnimation from "react-animate-on-scroll";
 import { TiTick } from "react-icons/ti";
 import Divider from "../Helper/Divider";
 
-
 const CounsellingSection = () => {
   const [randomIndex, setRandomIndex] = useState(0);
+  const [timelineMode, setTimelineMode] = useState("alternate");
   const counselingContent = [
     {
       title: "Understanding NEET Preparation Basics",
@@ -60,6 +60,20 @@ const CounsellingSection = () => {
     );
     return () => clearInterval(randomInterval);
   }, []);
+  useEffect(() => {
+    const updateMode = () => {
+      const currWidth = window.innerWidth;
+      console.log(currWidth)
+      if (currWidth < 800) {
+        setTimelineMode("left");
+      } else {
+        setTimelineMode("alternate");
+      }
+    };
+    updateMode();
+    window.addEventListener("resize", updateMode);
+    return () => window.removeEventListener("resize", updateMode);
+  }, []);
 
   return (
     <Container className="bg-gray-200/40">
@@ -67,51 +81,50 @@ const CounsellingSection = () => {
         <div className="mx-auto flex items-center justify-center rounded-md">
           <div>
             <div className="mb-4 text-center">
-              <h2 className="my-1 text-3xl font-bold text-yellow-600">
+              <h2 className="text-md my-1 font-bold text-yellow-600 lg:text-3xl">
                 What We Have To{" "}
-                <span className="font-extrabold text-primary-color">Offer ?</span>
+                <span className="font-extrabold text-primary-color">
+                  Offer ?
+                </span>
               </h2>
-              <p className="my-2 max-w-sm mx-auto text-xs font-semibold text-gray-700">
+              <p className="mx-auto my-2 max-w-72 text-xs font-semibold text-gray-700 lg:max-w-sm">
                 Get personalized answers and ease your biggest doubts with Video
                 Counselling from our curated experts.
               </p>
               <Divider
-            className={"mx-auto my-4 h-1 w-20 rounded-full bg-yellow-600"}
-          />
+                className={"mx-auto my-4 h-1 w-20 rounded-full bg-yellow-600"}
+              />
             </div>
 
             <Timeline
-              mode="alternate"
-              className="mx-auto home-timeline mt-10"
+              mode={timelineMode}
+              className="home-timeline mx-auto mt-8 lg:mt-10"
               items={counselingContent?.map((elem, index) => ({
                 children: (
-                  <ScrollAnimation
-                    animateIn={`backInUp`}
-                    duration={1.5}
-                  >
+                  <ScrollAnimation animateIn={`backInUp`} duration={1.5}>
                     <div
                       className={`relative mb-3 transform rounded-lg transition-all duration-200 ${
                         index % 2 === 0
-                          ? "flex w-[30.5rem] cursor-pointer bg-gradient-to-tr from-gray-100 to-blue-100/50 p-5 shadow-md hover:shadow-xl"
-                          : "mr-[30rem] flex w-[27.5rem] cursor-pointer bg-gradient-to-tr from-gray-100 to-blue-100/50 p-5 shadow-md hover:shadow-xl"
+                          ? "flex lg:w-[30.5rem] w-[20rem] cursor-pointer bg-gradient-to-tr from-gray-100 to-blue-100/50 p-5 shadow-md hover:shadow-xl"
+                          : "lg:mr-[30rem]  flex lg:w-[27.5rem] w-[20rem] cursor-pointer bg-gradient-to-tr from-gray-100 to-blue-100/50 p-5 shadow-md hover:shadow-xl"
                       } ${index == randomIndex ? "scale-105 border-l-2 border-yellow-600 transition-all" : "border-none"}`}
                     >
-                      <div className="my-auto flex-shrink-0 text-xl">
+                      <div className="my-auto flex-shrink-0 lg:text-xl text-[0.8rem] ">
                         <div
-                          className={`${index == randomIndex ? "bg-primary-color/20 text-primary-color" : "bg-primary-color text-white"} rounded-full p-3 transition-all duration-300 hover:bg-primary-color/20`}
+                          className={`${index == randomIndex ? "bg-primary-color/20 text-primary-color" : "bg-primary-color text-white"} rounded-full p-3   transition-all duration-300 hover:bg-primary-color/20`}
                         >
                           {elem?.icon}
                         </div>
                       </div>
-                      <div className="w-96 pl-4">
-                        <h1 className="pb-0.5 text-left text-lg font-bold text-gray-700">
+                      <div className="w-96  pl-4 ">
+                        <h1 className="pb-0.5 text-left lg:text-lg text-xs  font-extrabold lg:font-bold text-gray-700">
                           {elem?.title}
                         </h1>
-                        <ul className="my-1 flex w-[30rem] flex-col space-y-2">
+                        <ul className="my-1 flex lg:w-[30rem] w-full flex-col space-y-2">
                           {elem?.keyPoints?.map((point, idx) => (
                             <li key={idx} className="group flex items-center">
                               <TiTick className="text-xl text-green-500 transition-transform duration-200 group-hover:scale-110" />
-                              <span className="ml-2 text-left text-sm font-semibold text-gray-800">
+                              <span className="ml-2   text-left lg:text-sm text-xs font-semibold text-gray-800">
                                 {point}
                               </span>
                             </li>
