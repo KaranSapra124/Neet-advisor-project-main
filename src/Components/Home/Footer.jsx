c363a8ef89041aac9379636adc2b85421eff56ca
karansapra124 <sapra7112@gmail.com>
2025-02-19 17:30:07 +0530

    Responsiveness added to footer of homepage

--- src/Components/Home/Footer.jsx
+++ src/Components/Home/Footer.jsx
@@ -3,12 +3,14 @@ import React from "react";
 const Footer = () => {
   return (
     <footer
-      className={`${window.location.pathname.includes("medical-expo") || window.location.pathname.includes("pg-seminar") ? "bg-gradient-to-tr from-black/80 via-gray-900 to-gray-900/80" : window.location.pathname.includes("mbbs-abroad") ? "bg-gradient-to-br from-black/80 to-black/70" : window.location.pathname.includes("gallery") ? "bg-gradient-to-tr from-black/15   to-gray-900/50" : window.location.pathname.includes("ug-seminar") ? "bg-black/95" : window.location.pathname.includes("careers") ? "bg-gradient-to-br from-black/50 via-gray-700 to-black/90" : "bg-primary-color"} p-20 text-white`}
+      className={`${window.location.pathname.includes("medical-expo") || window.location.pathname.includes("pg-seminar") ? "bg-gradient-to-tr from-black/80 via-gray-900 to-gray-900/80" : window.location.pathname.includes("mbbs-abroad") ? "bg-gradient-to-br from-black/80 to-black/70" : window.location.pathname.includes("gallery") ? "bg-gradient-to-tr from-black/15 to-gray-900/50" : window.location.pathname.includes("ug-seminar") ? "bg-black/95" : window.location.pathname.includes("careers") ? "bg-gradient-to-br from-black/50 via-gray-700 to-black/90" : "bg-primary-color"} p-5 text-white lg:p-20`}
     >
-      <div className="container mx-auto flex flex-col items-center justify-between md:flex-row">
-        <div className="mb-4 md:mb-0">
-          <h3 className={`mb-2 text-lg font-bold text-white`}>Stay Updated!</h3>
-          <p className="text-sm">
+      <div className="mx-auto flex flex-col items-center md:flex-row lg:justify-between">
+        <div className="mb-2 md:mb-0 lg:mb-4">
+          <h3 className={`mb-2 text-sm font-bold text-white lg:text-lg`}>
+            Stay Updated!
+          </h3>
+          <p className="text-xs lg:text-sm">
             Get the latest NEET UG and NEET PG Counselling updates, Policies,
             News, Articles, and Analysis in your inbox.
           </p>
@@ -23,70 +25,76 @@ const Footer = () => {
             </button>
           </div>
         </div>
-        <div className="mb-4 md:mb-0">
-          <h3 className={`mb-2 text-lg font-bold text-white`}>Company</h3>
-          <ul>
-            <li className="text-sm">
-              <a href="#" className="hover:underline">
-                About us
-              </a>
-            </li>
-            <li className="text-sm">
-              <a href="#" className="hover:underline">
-                Contact us
-              </a>
-            </li>
-            <li className="text-sm">
-              <a href="#" className="hover:underline">
-                Careers
-              </a>
-            </li>
-            <li className="text-sm">
-              <a href="#" className="hover:underline">
-                Partnership
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div className="mb-4 md:mb-0">
-          <h3 className={`mb-2 text-lg font-bold text-white`}>Services</h3>
-          <ul>
-            <li className="text-sm">
-              <a href="#" className="hover:underline">
-                Attend Webinar
-              </a>
-            </li>
-            <li className="text-sm">
-              <a href="#" className="hover:underline">
-                Book a Session
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div className="mb-4 md:mb-0">
-          <h3 className={`mb-2 text-lg font-bold text-white`}>Social Media</h3>
-          <ul>
-            <li className="text-sm">
-              <a href="#" className="hover:underline">
-                Facebook
-              </a>
-            </li>
-            <li className="text-sm">
-              <a href="#" className="hover:underline">
-                Instagram
-              </a>
-            </li>
-            <li className="text-sm">
-              <a href="#" className="hover:underline">
-                Twitter
-              </a>
-            </li>
-            <li className="text-sm">
-              <a href="#" className="hover:underline">
-                LinkedIn
-              </a>
-            </li>
-          </ul>
+        <div className="flex w-full flex-row justify-between gap-2  lg:items-center lg:justify-around">
+          <div className="my-5 md:mb-0 lg:my-4">
+            <h3 className={`mb-2 text-sm font-bold text-white lg:text-lg`}>
+              Company
+            </h3>
+            <ul>
+              <li className="lg:text-sm">
+                <a href="#" className="text-xs hover:underline lg:text-sm">
+                  About us
+                </a>
+              </li>
+              <li className="text-sm">
+                <a href="#" className="text-xs hover:underline lg:text-sm">
+                  Contact us
+                </a>
+              </li>
+              <li className="text-sm">
+                <a href="#" className="text-xs hover:underline lg:text-sm">
+                  Careers
+                </a>
+              </li>
+              <li className="text-sm">
+                <a href="#" className="text-xs hover:underline lg:text-sm">
+                  Partnership
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="my-5 md:mb-0 lg:my-4">
+            <h3 className={`mb-2 text-sm font-bold text-white lg:text-lg`}>Services</h3>
+            <ul>
+              <li className="text-sm">
+                <a href="#" className="text-xs hover:underline lg:text-sm">
+                  Attend Webinar
+                </a>
+              </li>
+              <li className="text-sm">
+                <a href="#" className="text-xs hover:underline lg:text-sm">
+                  Book a Session
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="my-5 md:mb-0 lg:my-4">
+            <h3 className={`mb-2 text-sm font-bold text-white lg:text-lg`}>
+              Social Media
+            </h3>
+            <ul>
+              <li className="text-sm">
+                <a href="#" className="text-xs hover:underline lg:text-sm">
+                  Facebook
+                </a>
+              </li>
+              <li className="text-sm">
+                <a href="#" className="text-xs hover:underline lg:text-sm">
+                  Instagram
+                </a>
+              </li>
+              <li className="text-sm">
+                <a href="#" className="text-xs hover:underline lg:text-sm">
+                  Twitter
+                </a>
+              </li>
+              <li className="text-sm">
+                <a href="#" className="text-xs hover:underline lg:text-sm">
+                  LinkedIn
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </footer>
