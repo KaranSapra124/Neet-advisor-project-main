79502d0cce2f0bce25745d171c99296b6f2c68b8
karansapra124 <sapra7112@gmail.com>
2025-02-19 10:33:56 +0530

    Gallery Improved

--- src/Components/Gallery/HappyClients.jsx
+++ src/Components/Gallery/HappyClients.jsx
@@ -1,6 +1,7 @@
 import { Carousel, Spin } from "antd";
 import React, { useEffect, useState } from "react";
 import Divider from "../Helper/Divider";
+import GlobalImageComponent from "../Helper/GlobalImageComponent";
 
 const HappyClients = () => {
   let count = 0;
@@ -106,36 +107,8 @@ const HappyClients = () => {
       </div>
 
       {!isLoading ? (
-        <div
-          className={`flex flex-col max-w-screen-xl`}
-        >
-          {partitions?.map((_, index) => {
-            return (
-              <Carousel
-                autoplay
-                autoplaySpeed={2000}
-                rtl={index % 2 === 0 ? true : false}
-                dots={false}
-                // vertical={true}
-                slidesToShow={3}
-                infinite
-              >
-                {images
-                  ?.slice(
-                    partitions[index],
-                    index + 1 < partitions.length
-                      ? partitions[index + 1] + 1
-                      : partitions.length,
-                  )
-                  ?.map((ele, ind) => {
-                    // {console.log(ele,ind,"FIRST")}
-                    return (
-                      <img className="m-2 shadow-md shadow-white h-64 w-64" src={ele?.imageUrl} />
-                    );
-                  })}
-              </Carousel>
-            );
-          })}
+        <div className={`flex max-w-screen-xl flex-col`}>
+          <GlobalImageComponent image={images} />
         </div>
       ) : (
         <div className="flex items-center justify-center">
