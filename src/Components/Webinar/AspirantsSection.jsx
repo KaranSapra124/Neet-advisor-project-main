0252633bcb5e6285de81ef0b6004656a95a02906
karansapra124 <sapra7112@gmail.com>
2025-02-20 14:24:01 +0530

    Fixed responsive issues in aspirantsSection of webinar

--- src/Components/Webinar/AspirantsSection.jsx
+++ src/Components/Webinar/AspirantsSection.jsx
@@ -310,13 +310,13 @@ const AspirantsSection = () => {
   return (
     <Container className={"bg-gray-200/40  lg:py-0"}>
       <div className="mb-4 lg:max-w-screen-xl lg:text-center">
-        <h1 className="mb-1.5 text-[0.8rem] font-bold text-yellow-600 lg:mb-2 lg:text-3xl">
+        <h1 className="mb-1.5 text-[0.8rem] max-[380px]:text-[0.7rem] max-[380px]:text-center font-bold text-yellow-600 lg:mb-2 lg:text-3xl">
           Students Coached to{" "}
           <span className="font-extrabold text-primary-color">
             Top Medical Colleges
           </span>
         </h1>
-        <p className="mx-auto my-2 text-[0.6rem] font-bold text-gray-800 lg:my-4 lg:max-w-xl lg:text-xs">
+        <p className="mx-auto max-[380px]:text-center my-2 text-[0.6rem] font-bold text-gray-800 lg:my-4 lg:max-w-xl lg:text-xs">
           Check out the medical colleges our attendees have reached with
           enhanced confidence, clarity, and applying our strategies.
         </p>
