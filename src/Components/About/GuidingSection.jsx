4b18459a9fc20091af5ebe4fce7cb7f4b69f743b
karansapra124 <sapra7112@gmail.com>
2025-02-21 11:34:45 +0530

    Completed Responsiveness of guiding section in about us

--- src/Components/About/GuidingSection.jsx
+++ src/Components/About/GuidingSection.jsx
@@ -20,7 +20,7 @@ const GuidingSection = () => {
 
       <ScrollAnimation animateIn="backInRight" duration={1.5}>
         <div className="relative z-50 mx-auto">
-          <h1 className="text-center lg:text-3xl text-sm font-bold text-yellow-600">
+          <h1 className="text-center text-sm font-bold text-yellow-600 lg:text-3xl">
             Guiding you towards your{" "}
             <span className="relative inline-block">
               <span className="font-extrabold text-primary-color">
@@ -29,25 +29,32 @@ const GuidingSection = () => {
               {/* <div className="absolute mx-auto -bottom-2 left-0 h-1 w-full rounded-full bg-yellow-600"></div> */}
             </span>
           </h1>
-          <p className="text-gray-800 font-semibold text-center lg:text-xs text-[0.5rem] lg:my-4 my-2  lg:max-w-xl mx-auto">Guiding you towards your dream career with personalized advice, expert guidance, and a clear roadmap to success.</p>
-          <Divider className={"lg:h-1 h-0.5 lg:w-20 w-12 rounded-full lg:my-4 my-2 bg-yellow-600 mx-auto"}/>
+          <p className="mx-auto my-2 text-center text-[0.5rem] font-semibold text-gray-800 lg:my-4 lg:max-w-xl lg:text-xs">
+            Guiding you towards your dream career with personalized advice,
+            expert guidance, and a clear roadmap to success.
+          </p>
+          <Divider
+            className={
+              "mx-auto my-2 h-0.5 w-12 rounded-full bg-yellow-600 lg:my-4 lg:h-1 lg:w-20"
+            }
+          />
 
           {/* <div className="h-52 top-10 relative max-w-sm border border-black mx-auto"></div> */}
-          <div className="flex ">
-            <div className="mx-auto flex  lg:flex-row flex-col h-full w-full items-center justify-center lg:gap-8 gap-4">
+          <div className="flex">
+            <div className="mx-auto flex h-full w-full flex-col items-center justify-center gap-4 lg:flex-row lg:gap-8">
               {/* First Card */}
-              <div className="group h-full lg:w-96 lg:h-60  transform cursor-pointer rounded-md border-b-2 border-l-2 border-yellow-600 bg-white shadow-lg transition-transform duration-300 hover:scale-x-105 hover:shadow-xl">
-                <div className="flex flex-col items-center lg:p-6 p-3  text-center">
+              <div className="group h-full transform cursor-pointer rounded-md border-b-2 border-l-2 border-yellow-600 bg-white shadow-lg transition-transform duration-300 hover:scale-x-105 hover:shadow-xl lg:h-60 lg:w-96">
+                <div className="flex flex-col items-center p-3 text-center lg:p-6">
                   <div className="relative mb-4">
                     <div className="absolute inset-0 animate-pulse rounded-full bg-primary-color/10"></div>
                     <img
                       src="./About/Student.gif"
-                      className="lg:h-12 lg:w-12 h-6 w-6 rounded-full shadow-md shadow-yellow-600 group-hover:scale-105"
+                      className="h-6 w-6 rounded-full shadow-md shadow-yellow-600 group-hover:scale-105 lg:h-12 lg:w-12"
                       alt=""
                       srcset=""
                     />
                   </div>
-                  <p className="lg:text-xs text-[0.5rem] font-semibold text-justify leading-relaxed text-gray-800">
+                  <p className="text-justify text-[0.5rem] font-semibold leading-relaxed text-gray-800 lg:text-xs">
                     At NEET advisor, we are a team of young professionals
                     dedicated to resolving your doubts about NEET Counselling
                     and supporting you in the crucial application process,
@@ -57,7 +64,7 @@ const GuidingSection = () => {
                 </div>
               </div>
               <Carousel
-                className="w-[30rem]"
+                className="max-w-xs lg:w-[30rem]"
                 dots={false}
                 autoplay
                 autoplaySpeed={2500}
@@ -67,18 +74,18 @@ const GuidingSection = () => {
                 })}
               </Carousel>
               {/* Second Card */}
-              <div className="h-full w-96 transform cursor-pointer rounded-md border-b-2 border-l-2 border-yellow-600 bg-white shadow-lg transition-transform duration-300 hover:scale-x-105 hover:shadow-xl">
-                <div className="flex flex-col items-center p-6 text-center">
+              <div className="group h-full transform cursor-pointer rounded-md border-b-2 border-l-2 border-yellow-600 bg-white shadow-lg transition-transform duration-300 hover:scale-x-105 hover:shadow-xl lg:h-60 lg:w-96">
+                <div className="flex flex-col items-center lg:p-6 p-3 text-center">
                   <div className="relative mb-4">
                     <div className="absolute inset-0 animate-pulse rounded-full bg-primary-color/10"></div>
                     <img
                       src="./About/person-speaker.gif"
-                      className="h-12 w-12 rounded-full shadow-md shadow-yellow-600"
+                      className="h-6 w-6 rounded-full shadow-md shadow-yellow-600 group-hover:scale-105 lg:h-12 lg:w-12"
                       alt=""
                       srcset=""
                     />{" "}
                   </div>
-                  <p className="text-xs font-semibold text-justify leading-relaxed text-gray-800">
+                  <p className="text-justify text-[0.5rem] font-semibold leading-relaxed text-gray-800 lg:text-xs">
                     Our innovative offerings have been exclusively designed for
                     NEET UG and NEET PG aspirants. Our programs are like having
                     your own personal coach who will empower you with all the
