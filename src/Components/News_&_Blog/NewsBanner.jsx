d4a02eec51d8966041612349ea3a092c833fff34
karansapra124 <sapra7112@gmail.com>
2025-02-22 11:58:43 +0530

    Added Responsiveness to News Banner section News&Blog

--- src/Components/News_&_Blog/NewsBanner.jsx
+++ src/Components/News_&_Blog/NewsBanner.jsx
@@ -5,26 +5,26 @@ import { FaPaperPlane } from "react-icons/fa";
 const NewsBanner = () => {
   return (
     <div className="h-full bg-gray-200/40">
-      <div className="relative mx-auto max-w-screen-xl rounded-lg bg-news-bg bg-cover bg-no-repeat shadow-sm shadow-white">
+      <div className="relative mx-auto lg:max-w-screen-xl max-w-80 rounded-lg bg-news-bg bg-cover bg-no-repeat shadow-sm shadow-white">
         <div className="absolute inset-0 h-full w-full rounded-lg bg-black/80"></div>
-        <Container className={"relative brightness-110"}>
+        <Container className={"relative   brightness-110"}>
           <img
             src="call.gif"
-            className="absolute -left-5 -top-5 h-10 w-10 rounded-full shadow-md shadow-yellow-600"
+            className="absolute lg:-left-5 lg:-top-5 -top-4 -left-3 lg:h-10 lg:w-10 w-7 rounded-full shadow-md shadow-yellow-600"
             alt=""
             srcset=""
           />
 
-          <h1 className="text-3xl font-extrabold uppercase text-yellow-600">
-            Get the desired <br />{" "}
-            <span className="text-primary-color brightness-200">
+          <h1 className="lg:text-3xl text-xs font-extrabold uppercase text-yellow-600">
+            Get the desired <br className="lg:block hidden"/>{" "}
+            <span className="text-primary-color lg:brightness-200 brightness-[300%]">
               news at one place
             </span>
           </h1>
-          <p className="my-4 text-xs font-semibold uppercase text-gray-300">
+          <p className="lg:my-4 my-2 lg:text-xs text-[0.5rem] font-semibold uppercase text-gray-300">
             Get All News Updates At Your Device.
           </p>
-          <Divider className={"my-4 h-1 w-12 rounded-full bg-yellow-600"} />
+          <Divider className={"lg:my-4 my-2 lg:h-1 h-0.5 w-12 rounded-full bg-yellow-600"} />
           <div className="flex items-center gap-2">
             <input
               type="text"
@@ -33,9 +33,9 @@ const NewsBanner = () => {
               name=""
               id=""
               placeholder="Enter The Number..."
-              className="w-96 rounded-md  bg-white/40 p-2 font-extralight text-primary-color"
+              className="lg:w-96 w-full lg:text-sm text-xs rounded-md  bg-white/40 p-2 font-extralight text-primary-color"
             />
-            <FaPaperPlane className="bg-yellow-600 p-4 rounded-md text-[3rem] cursor-pointer hover:scale-105 transition-all text-white"/>
+            <FaPaperPlane className="bg-yellow-600 lg:p-4 p-2 rounded-md text-[2rem] lg:text-[3rem] cursor-pointer hover:scale-105 transition-all text-white"/>
           </div>
         </Container>
       </div>
