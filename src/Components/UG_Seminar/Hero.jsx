0f0379eaac3f1f1e6dd3091c6b7a24eb525786c1
karansapra124 <sapra7112@gmail.com>
2025-02-22 10:46:14 +0530

    Added Responsiveness to hero section ug-seminar

--- src/Components/UG_Seminar/Hero.jsx
+++ src/Components/UG_Seminar/Hero.jsx
@@ -26,7 +26,7 @@ const Hero = () => {
   ];
   return (
     <>
-      <Container className={"relative h-screen"}>
+      <Container className={"relative lg:h-screen h-full"}>
         <video
           src="./Seminar/Seminar-video.mp4"
           autoPlay
@@ -35,29 +35,29 @@ const Hero = () => {
           className="absolute inset-0 h-full w-full object-cover"
         ></video>
         <div className="absolute inset-0 h-full w-full bg-gradient-to-r from-black/90 to-black/50"></div>
-        <div className="relative flex h-full w-full items-center justify-between">
+        <div className="relative flex lg:flex-row flex-col h-full w-full items-center justify-between">
           <ScrollAnimation
             animateIn="backInLeft"
             duration={1.5}
             animateOnce={true}
             delay={1}
           >
-            <div className="flex flex-col gap-5">
-              <div className="w-fit rounded-l-full rounded-r-full border border-gray-700 bg-yellow-600 px-2 py-1 text-sm font-bold uppercase text-yellow-300">
+            <div className="flex flex-col lg:gap-5 gap-2.5">
+              <div className="w-fit rounded-l-full rounded-r-full border border-gray-700 bg-yellow-600 px-2 py-1 lg:text-sm text-xs font-bold uppercase text-yellow-300">
                 #1 Neet counsellor
               </div>
-              <h1 className="text-4xl  font-extrabold leading-tight text-yellow-600">
+              <h1 className="lg:text-4xl text-lg  font-extrabold leading-tight text-yellow-600">
                 Fuel Your Dream <br /> of Becoming a Doctor <br />
-                <span className="text-6xl font-extrabold text-primary-color brightness-200">
+                <span className="lg:text-6xl text-3xl font-extrabold text-primary-color brightness-200">
                   NEET UG 2025
                 </span>
               </h1>
-              <p className="max-w-md text-xs font-extrabold text-gray-200">
+              <p className="lg:max-w-md lg:text-xs text-[0.6rem] font-extrabold text-gray-200">
                 Prepare yourself for the NEET UG exam with expert guidance and
                 tips. Join us for insightful sessions, mock tests, and career
                 counseling.
               </p>
-              <button className="bg-yellow-600 w-fit rounded-l-full rounded-r-full transition-all px-3 py-2 font-bold text-white hover:bg-yellow-700">
+              <button className="bg-yellow-600 lg:text-sm text-xs w-fit rounded-l-full rounded-r-full transition-all lg:px-3 lg:py-2 px-2 py-1 font-bold text-white hover:bg-yellow-700">
                 Book Now!
               </button>
             </div>
@@ -69,29 +69,29 @@ const Hero = () => {
             animateOnce={true}
             delay={1}
           >
-            <div className="h-fit rounded-md bg-gray-200/10 p-8 shadow-md shadow-white/50 backdrop-blur-sm">
+            <div className="h-fit rounded-md bg-gray-200/10 lg:p-8 p-4 lg:my-0 my-4 shadow-md shadow-white/50 backdrop-blur-sm">
               {seminarData?.map((seminar, index) => {
                 return (
                   <div className="cursor-pointer transition-all duration-100 hover:scale-105">
-                    <div className="w-fit rounded-l-full rounded-r-full bg-yellow-600 px-2 py-1 text-sm font-bold uppercase text-yellow-200">
+                    <div className="w-fit rounded-l-full rounded-r-full bg-yellow-600 lg:px-2 px-1.5 py-1 lg:text-sm text-[0.6rem] font-bold uppercase text-yellow-200">
                       # {seminar?.location}
                     </div>
-                    <div className="flex items-center justify-between py-2">
-                      <h1 className="text-3xl font-bold text-gray-200">
+                    <div className="flex items-center justify-between lg:py-2 py-1.5">
+                      <h1 className="lg:text-3xl text-sm lg:font-bold font-extrabold text-gray-200">
                         {seminar?.date}
                       </h1>
                       <div
-                        className={`w-fit mx-4 rounded-md ${seminar?.isAvailable ? "border-2 border-primary-color bg-white/50 text-primary-color" : "border-2 border-gray-800 bg-white/50 text-gray-800"} px-2 py-1 text-sm font-bold uppercase`}
+                        className={`w-fit lg:mx-4 mx-2 rounded-md ${seminar?.isAvailable ? "border-2 border-primary-color bg-white/50 text-primary-color" : "border-2 border-gray-800 bg-white/50 text-gray-800"} lg:px-2 px-1.5 lg:py-1 py-0.5 lg:text-sm text-[0.7rem] lg:font-bold font-extrabold uppercase`}
                       >
                         {seminar?.isAvailable ? "Available" : "Not Available"}
                       </div>
-                      <FaArrowRight className="animate-scaleUp text-yellow-600" />
+                      <FaArrowRight className="animate-scaleUp lg:text-sm text-xs text-yellow-600" />
                     </div>
-                    <div className="text-xs font-semibold text-gray-200 ">
+                    <div className="lg:text-xs text-[0.6rem] font-semibold text-gray-200 ">
                       {seminar.venue}
                     </div>
                     {index !== seminarData?.length - 1 && (
-                      <Divider className="my-5 h-0.5 w-full rounded-full bg-yellow-600" />
+                      <Divider className="lg:my-5 my-2.5 h-0.5 w-full rounded-full bg-yellow-600" />
                     )}
                   </div>
                 );
