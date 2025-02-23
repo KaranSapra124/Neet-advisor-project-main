9ea87133ce316395d0fa9572ffef689de1f2307a
karansapra124 <sapra7112@gmail.com>
2025-02-23 12:31:10 +0530

    Added responsiveness to Apply for dream university  of mbbs abroad page

--- src/Components/Mbbs_Abroad/ApplyForDreamUniversity.jsx
+++ src/Components/Mbbs_Abroad/ApplyForDreamUniversity.jsx
@@ -5,37 +5,53 @@ import Divider from "../Helper/Divider";
 const ApplyForDreamUniversity = () => {
   const universities = [
     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfIKkCC_hPP54YE4ckBhp1tLUHA9UxjXnFIw&s",
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRc9CHrWVSlfLaZdv1Z8gHBwiAx-YiYMdSOKA&s',
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIeRI1VmXFwBGt96AW8K1OyXIgNoMuzjnZeQ&s',
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoLe9XasaFDlE6FB5xI-Csx8yKiBbLvI2L7g&s',
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRc9CHrWVSlfLaZdv1Z8gHBwiAx-YiYMdSOKA&s",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIeRI1VmXFwBGt96AW8K1OyXIgNoMuzjnZeQ&s",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoLe9XasaFDlE6FB5xI-Csx8yKiBbLvI2L7g&s",
     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfIKkCC_hPP54YE4ckBhp1tLUHA9UxjXnFIw&s",
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRc9CHrWVSlfLaZdv1Z8gHBwiAx-YiYMdSOKA&s',
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIeRI1VmXFwBGt96AW8K1OyXIgNoMuzjnZeQ&s',
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoLe9XasaFDlE6FB5xI-Csx8yKiBbLvI2L7g&s',
-    
-  ]
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRc9CHrWVSlfLaZdv1Z8gHBwiAx-YiYMdSOKA&s",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIeRI1VmXFwBGt96AW8K1OyXIgNoMuzjnZeQ&s",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoLe9XasaFDlE6FB5xI-Csx8yKiBbLvI2L7g&s",
+  ];
   return (
     <>
       <Container
-        className={"bg-mbbs-hero relative bg-cover bg-fixed bg-no-repeat"}
+        className={"relative bg-mbbs-hero bg-cover bg-fixed bg-no-repeat"}
       >
-        <div className="absolute inset-0 h-full w-full bg-gradient-to-br from-black/80 to-black/70 backdrop-blur-lg"></div>
+        <div className="absolute inset-0 h-full w-full bg-gradient-to-br from-black/80 to-black/50 backdrop-blur-lg lg:to-black/70"></div>
 
         <div className="relative">
-          <h1 className="text-center text-3xl font-bold text-yellow-600">
+          <h1 className="text-center text-lg font-bold text-yellow-600 lg:text-3xl">
             Get University Of{" "}
-            <span className="font-extrabold text-primary-color brightness-200">Your Dream</span>{" "}
+            <span className="text-xl font-extrabold text-primary-color lg:text-3xl lg:brightness-200">
+              Your Dream
+            </span>{" "}
           </h1>
-          <p className="my-4 text-center text-xs font-semibold text-gray-200">
+          <p className="my-2 text-center text-[0.5rem] font-semibold text-gray-200 lg:my-4 lg:text-xs">
             Want to get admission at your dream university , your wish is
             granted!
           </p>
-          <Divider className={"h-1 w-12 my-4 mx-auto bg-yellow-600 rounded-full"}/>
-          <Carousel autoplay autoplaySpeed={2000} dots={false} arrows infinite  slidesToShow={5} className="my-4">
+          <Divider
+            className={
+              "mx-auto my-2 h-0.5 w-12 rounded-full bg-yellow-600 lg:my-4 lg:h-1"
+            }
+          />
+          <Carousel
+            autoplay
+            autoplaySpeed={2000}
+            dots={false}
+            arrows
+            infinite
+            slidesToShow={window.outerWidth > 800 ? 5 : 1}
+            className="my-4"
+          >
             {universities?.map((elem, index) => {
               return (
                 <>
-                  <img className="mx-auto outline outline-yellow-600 p-1  w-32 h-32 rounded-full my-4" src={elem} />
+                  <img
+                    className="mx-auto lg:my-4 my-2 lg:h-32 lg:w-32 h-24 w-24 rounded-full p-1 outline outline-yellow-600"
+                    src={elem}
+                  />
                 </>
               );
             })}
