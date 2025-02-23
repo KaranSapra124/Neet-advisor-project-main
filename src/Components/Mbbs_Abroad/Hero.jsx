93a07dad9f7362b2448fe269c26228e5572bd32d
karansapra124 <sapra7112@gmail.com>
2025-02-23 11:36:52 +0530

    Added responsiveness to hero of mbbs abroad page

--- src/Components/Mbbs_Abroad/Hero.jsx
+++ src/Components/Mbbs_Abroad/Hero.jsx
@@ -20,15 +20,15 @@ const Hero = () => {
 
   const features = [
     {
-      icon: <FaGlobeAmericas className="mx-auto h-6 w-6 text-white" />,
+      icon: <FaGlobeAmericas className="mx-auto lg:h-6 lg:w-6 w-8 text-white" />,
       text: "International Education",
     },
     {
-      icon: <FaBookReader className="mx-auto h-6 w-6 text-white" />,
+      icon: <FaBookReader className="mx-auto lg:h-6 lg:w-6 w-8 text-white" />,
       text: "Expert Counselling",
     },
     {
-      icon: <FaUserGraduate className="mx-auto h-6 w-6 text-white" />,
+      icon: <FaUserGraduate className="mx-auto lg:h-6 lg:w-6 w-8 text-white" />,
       text: "Career Guidance",
     },
   ];
@@ -50,28 +50,28 @@ const Hero = () => {
         duration={1.5}
         initiallyVisible
       >
-        <Container className={"relative h-screen"}>
+        <Container className={"relative lg:h-screen h-[32rem] "}>
           {/* Image Section */}
           <img
             key={currentImage}
             src={imageArr[currentImage]}
             className="absolute inset-0 h-full w-full animate-fadeIn object-cover brightness-110"
           />
-          <div className="absolute inset-0 h-full w-full bg-black/35"></div>
-          <div className="flex h-full items-center">
-            <div className="relative flex h-fit w-fit flex-col justify-center gap-5 rounded-md bg-black/70 p-4 shadow-sm shadow-white backdrop-blur-none">
-              <div className="w-fit rounded-l-full rounded-r-full bg-yellow-600 px-2 py-1 text-sm font-extrabold uppercase text-white">
+          <div className="absolute inset-0  h-full w-full lg:bg-black/35 bg-black/20"></div>
+          <div className="flex h-full lg:items-center items-end">
+            <div className="relative flex h-fit w-fit flex-col justify-center lg:gap-5 gap-2 rounded-md lg:bg-black/70 bg-black/80 lg:p-4 p-2 shadow-sm shadow-white backdrop-blur-none">
+              <div className="w-fit rounded-l-full rounded-r-full bg-yellow-600 px-2 py-1 lg:text-sm text-xs font-extrabold uppercase text-white">
                 # Neet advisor
               </div>
-              <h1 className="text-3xl font-extrabold text-yellow-600 brightness-110">
-                Your Gateway to Pursuing MBBS Abroad,
-                <br />
-                <span className="text-primary-color brightness-[250%]">
+              <h1 className="lg:text-3xl text-md font-extrabold text-yellow-600 brightness-110">
+                Your Gateway to Pursuing MBBS Abroad ,
+                <br className="lg:block hidden"/>
+                <span className="text-primary-color lg:brightness-[250%] brightness-[250%]">
                   {" "}
                   Achieve Your Medical Dreams Globally!
                 </span>
               </h1>
-              <p className="max-w-screen-sm text-justify text-xs font-semibold text-gray-200">
+              <p className="lg:max-w-screen-sm text-justify lg:text-xs text-[0.5rem] font-semibold text-gray-200">
                 Pursuing an MBBS abroad opens doors to world-class medical
                 education, cutting-edge facilities, and global exposure. With
                 opportunities to study in renowned universities across countries
@@ -83,28 +83,28 @@ const Hero = () => {
                 reality!
               </p>
               <div className="flex justify-start">
-                <button className="text-sm rounded-l-full rounded-r-full bg-primary-color px-2 py-1 font-bold  text-white transition-all hover:scale-105">
+                <button className="lg:text-sm text-[0.7rem] rounded-l-full rounded-r-full bg-primary-color px-2 py-1 font-bold  text-white transition-all hover:scale-105">
                   Register
                 </button>
-                <button className="text-sm mx-2 rounded-l-full rounded-r-full border-2 border-gray-200 px-2 py-1 font-bold  text-gray-200 transition-all duration-200 hover:scale-110 hover:border-none hover:bg-black hover:text-white">
+                <button className="lg:text-sm text-[0.7rem] mx-2 rounded-l-full rounded-r-full border-2 border-gray-200 px-2 py-1 font-bold  text-gray-200 transition-all duration-200 hover:scale-110 hover:border-none hover:bg-black hover:text-white">
                   Learn More
                 </button>
               </div>
             </div>
-            <div className="absolute left-[20.5rem] top-[31rem] flex gap-2">
+            <div className="absolute lg:left-[20.5rem] left-[12.5rem]  lg:top-[31rem] top-[27rem]  flex gap-2">
               <Carousel
                 autoplay
                 infinite
-                slidesToShow={3}
+                slidesToShow={window.outerWidth > 800 ? 3 : 1}
                 dots={false}
-                className="max-w-screen-sm"
+                className="lg:max-w-screen-sm max-w-40"
               >
                 {features?.map((elem, index) => {
                   return (
-                    <div className="flex h-20 w-32 flex-col items-center justify-center space-y-4 rounded-md border border-white bg-black/75 p-2 text-center shadow-md">
+                    <div className="flex lg:h-20  lg:w-32  w-fit flex-col items-center justify-center lg:space-y-4 space-y-2 rounded-md border border-white bg-black/75 lg:p-2 p-1.5 text-center shadow-md">
                       <div className="m-auto">{elem?.icon}</div>
                       {/* <Divider className="h-0.5 w-2 bg-yellow-600"/> */}
-                      <p className="text-sm font-semibold text-gray-200">
+                      <p className="lg:text-sm text-xs font-semibold text-gray-200">
                         {elem?.text}
                       </p>
                     </div>
