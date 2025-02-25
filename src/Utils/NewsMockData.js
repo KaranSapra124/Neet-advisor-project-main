178652800ef8c5c5be1c00a0de4061037e784b4e
karansapra124 <sapra7112@gmail.com>
2025-02-25 18:22:54 +0530

    Started Building mock data for news

---
+++ src/Utils/NewsMockData.js
@@ -0,0 +1,9 @@
+export const newsData = {
+  title:
+    "NEET Application Form 2025 (OUT): Registration Link, Last Date to Apply at neet.nta.nic.in",
+  editor: "Karan Sapra",
+  date: "25 February 2025",
+  registration_link: "https://neet.nta.nic.in",
+  status: "OUT",
+  exam: "NEET 2025",
+};
