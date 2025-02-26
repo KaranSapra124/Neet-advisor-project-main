a448abcf918fa8a2ce74d6e2e963350f57a9deaa
karansapra124 <sapra7112@gmail.com>
2025-02-26 12:53:28 +0530

    Hero section news page done

--- src/Utils/NewsMockData.js
+++ src/Utils/NewsMockData.js
@@ -11,4 +11,13 @@ export const newsData = {
     status: false,
   },
   categories: ["Education", "Exams", "Medical Entrance", "NEET"],
+  heading:
+    "The NTA has released the NEET application form 2025. The NEET application form date is February 7, 2025, Students can fill the NEET 2025 application form at the official website, neet.nta.nic.in. The NEET application form last date is March 7, 2025. Candidates need to complete their NEET registration 2025 first before filling out the online application form.  Only such candidates who fulfil the eligibility criteria can fill NEET UG application form. ",
+  storyData: [
+    "NEET 2025 application form date",
+    "Eligibility criteria for NEET application form 2025",
+    "How to fill NEET UG 2025 application form?",
+    "NEET 2025 application form - Specification of scanned documents",
+    "NEET application form 2025 documents required",
+  ],
 };
