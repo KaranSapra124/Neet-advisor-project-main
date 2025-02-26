e13204adc1d2b28c6a5593793e0af5a17dcd4c10
karansapra124 <sapra7112@gmail.com>
2025-02-26 17:29:22 +0530

    Removed Unnecessary files from news

--- src/Components/News_&_Blog/EligibilityCriteria.jsx
+++ src/Components/News_&_Blog/EligibilityCriteria.jsx
@@ -5,15 +5,15 @@ const EligibilityCriteria = () => {
   return (
     <>
       <Container>
-        <div>
+        <div className="flex flex-col gap-2">
           <h1 className="text-xl text-yellow-600 font-bold"><span className="text-primary-color font-extrabold">Eligibility Criteria</span> For NEET Exam 2025</h1>
-          <p>
+          <p className="text-gray-700 font-semibold text-xs">
             Before filling out the NEET application form, aspirants must check
             the eligibility criteria set as per the qualification codes of NEET
             exam. Aspirants can check the NEET 2025 eligibility criteria from
             below.
           </p>
-          <h1>NEET 2025 application eligibility criteria</h1>
+          <h1 className="text-xl text-primary-color">NEET 2025 application eligibility criteria</h1>
         </div>
       </Container>
     </>
