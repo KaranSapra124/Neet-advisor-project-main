e7e416dfdacd7ad51f12580da2933007c70cb635
karansapra124 <sapra7112@gmail.com>
2025-02-26 15:25:30 +0530

    Added Improtant Links To NewsData

--- src/Components/News_&_Blog/MoreNews.jsx
+++ src/Components/News_&_Blog/MoreNews.jsx
@@ -4,26 +4,30 @@ import Divider from "../Helper/Divider";
 import { newsData } from "../../Utils/NewsMockData";
 
 const MoreNews = () => {
-  const { heading, storyData } = newsData;
+  const { heading, storyData, importantLinks } = newsData;
   return (
     <Container className={"bg-gray-200/10"}>
-      <div>
+      <div className="flex flex-col gap-2">
         <p className="text-sm font-thin text-gray-800">{heading}</p>
-        <Divider className={'w-12 h-1 rounded-full bg-yellow-600 my-4'}/>
-        <StoryContent storyData={storyData}/>
+        <Divider className={"my-4 h-1 w-12 rounded-full bg-yellow-600"} />
+        <StoryContent storyData={storyData} />
+        <ImportantLinks importantLinks={importantLinks} />
       </div>
-
     </Container>
   );
 };
 
-const StoryContent = ({storyData}) => {
+const StoryContent = ({ storyData }) => {
   return (
     <>
-      <div className="flex py-5 items-center justify-between">
-
-        <ol className="flex relative h-80 w-1/2 flex-col gap-3.5 rounded border bg-gray-200/10 px-5 py-4">
-        <img src="./Webinar/BookImg.gif" className="w-7 shadow shadow-yellow-600 rounded-full absolute -top-4 -left-4 bg-white" alt="" srcset="" />
+      <div className="flex items-center justify-between py-5">
+        <ol className="relative flex h-80 w-1/2 flex-col gap-3.5 rounded border bg-gray-200/10 px-5 py-4">
+          <img
+            src="./Webinar/BookImg.gif"
+            className="absolute -left-4 -top-4 w-7 rounded-full bg-white shadow shadow-yellow-600"
+            alt=""
+            srcset=""
+          />
           {storyData?.map((elem, index) => {
             return (
               <>
@@ -44,6 +48,41 @@ const StoryContent = ({storyData}) => {
           srcset=""
         />
       </div>
+    </>
+  );
+};
+
+const ImportantLinks = ({ importantLinks }) => {
+  return (
+    <>
+      {/* <div className="flex  w-full "> */}
+        {/* <img
+          className="w-1/2"
+          src="https://img.freepik.com/premium-vector/portrait-doctor-one-line-drawing-isolated-vector_566661-900.jpg?ga=GA1.1.715862983.1733989251&semt=ais_hybrid"
+          alt=""
+          srcset=""
+        /> */}
+        <div className="w-full rounded flex flex-col gap-3.5 border py-4  px-5 relative bg-gray-200/10 ">
+        <img
+            src="./Webinar/BookImg.gif"
+            className="absolute -left-3 -top-4 w-7 rounded-full bg-white shadow shadow-yellow-600"
+            alt=""
+            srcset=""
+          />
+          {importantLinks?.map((elem, index) => {
+            {
+              return Object.keys(elem)?.map((key) => {
+                return (
+                  <li className="list-none text-primary-color">
+                    <span className="border-b border-yellow-600 font-thin mx-1 text-gray-900">{key}:</span>
+                    {elem[key]}
+                  </li>
+                );
+              });
+            }
+          })}
+        </div>
+      {/* </div> */}
     </>
   );
 };
