cdf5627c6ac6506fa29a3b77fa33d9618b72c53b
karansapra124 <sapra7112@gmail.com>
2025-02-27 15:32:35 +0530

    Added Section Of StepThree

--- src/Components/News_&_Blog/StepThree.jsx
+++ src/Components/News_&_Blog/StepThree.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
-
+import React from "react";
+import { newsData } from "../../Utils/NewsMockData";
+import Container from "../Helper/Container";
+import { Collapse } from "antd";
 const StepThree = () => {
+  const { stepThree } = newsData;
   return (
-    <div>StepThree</div>
-  )
-}
+    <Container>
+      <div>
+        <h1>
+          Uploading Of Scanned Documents & Images For NEET 2025 Registration
+          Form
+        </h1>
+        <p>
+          After filling in the details in the NEET form 2025, candidates have to
+          upload scanned copies of the photograph and signature as specified.
+          Candidates need to click on 'upload documents'. The documents to be
+          uploaded are detailed below:
+        </p>
+        <div>
+          <CollapseData data={stepThree} />
+        </div>
+      </div>
+    </Container>
+  );
+};
 
-export default StepThree
+const CollapseData = ({ data }) => {
+  return (
+    <>
+      <div>
+        {data?.map((elem, index) => {
+          return (
+            <>
+              <Collapse
+                defaultActiveKey={[0]}
+                className="mx-auto mb-2 w-96"
+                accordion
+                items={[
+                  {
+                    key: index,
+                    label: (
+                      <h1 className="font-semibold text-primary-color">
+                        {elem?.title}
+                      </h1>
+                    ),
+                    children: (
+                      <p className="rounded border p-1.5 text-xs font-semibold text-gray-700">
+                        {elem?.desc}
+                      </p>
+                    ),
+                  },
+                ]}
+              />
+            </>
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
+export default StepThree;
