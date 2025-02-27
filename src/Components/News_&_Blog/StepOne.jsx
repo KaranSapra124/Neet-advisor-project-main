cfb2ee2814268a106494eb5dea48e24b81a5ba04
karansapra124 <sapra7112@gmail.com>
2025-02-27 13:29:21 +0530

    Steps One Ended

--- src/Components/News_&_Blog/StepOne.jsx
+++ src/Components/News_&_Blog/StepOne.jsx
@@ -2,33 +2,52 @@ import React, { Children } from "react";
 import Container from "../Helper/Container";
 import { newsData } from "../../Utils/NewsMockData";
 import { Collapse } from "antd";
+import Divider from "../Helper/Divider";
 
 const StepOne = () => {
   const { neetDetails, stepOneDetails } = newsData;
   return (
     <>
       <Container>
-        <div className="flex flex-col">
-          <h1 className="text-yellow-600 text-xl font-bold">NEET <span>Registration 2025</span></h1>
-          <p>
+        <div className="flex flex-col gap-2">
+          <h1 className="text-xl font-bold text-yellow-600">
+            NEET{" "}
+            <span className="font-extrabold text-primary-color">
+              Registration 2025
+            </span>
+          </h1>
+          <p className="text-xs font-semibold text-gray-700">
             To fill the NEET application form 2025, candidates are required to
             click on 'fill application form'.On the new page, click on 'NTA NEET
             registration 2025 link' which will lead to the instructions page
             which must be read carefully.
           </p>
-          <p>
+          <p className="text-xs font-semibold text-gray-700">
             During NEET-UG registration process, candidates have to fill the
             below mentioned details.A password must also be created as per the
             given specifications which is used for all future login. A NEET
             application number is generated after registration for NEET NTA
             2025. This user ID is used to login to NEET form fill up 2025.
           </p>
-          <div>
-            {neetDetails?.map((elem, index) => {
-              return <li>{elem}</li>;
-            })}
+          <Divider className={"h-1 w-12 rounded-full bg-yellow-600"} />
+          <div className="flex w-full justify-between">
+            <div className="w-1/2">
+              <div className="rounded border bg-gray-100/80 p-2">
+                {neetDetails?.map((elem, index) => {
+                  return (
+                    <li className="list-none text-sm text-primary-color">
+                      <span className="mx-1 border-b border-yellow-600 font-semibold">
+                        {" "}
+                        {index + 1}.{" "}
+                      </span>
+                      {elem}
+                    </li>
+                  );
+                })}
+              </div>
+            </div>
+            <StepOneDetails data={stepOneDetails} />
           </div>
-          <StepOneDetails data={stepOneDetails} />
         </div>
       </Container>
     </>
@@ -36,18 +55,35 @@ const StepOne = () => {
 };
 
 const StepOneDetails = ({ data }) => {
+  // console.log(data)
   return (
     <>
-      <div>
-        <Collapse accordion 
-          items={data?.map((elem, index) => {
-            return {
-              key: index,
-              label: elem?.title,
-              children: <p>{elem?.desc}</p>,
-            };
-          })}
-        />
+      <div className="w-1/2">
+        {data?.map((elem, index) => {
+          console.log(elem);
+          return (
+            <Collapse
+              defaultActiveKey={[0]}
+              className="mx-auto my-2 w-96"
+              accordion
+              items={[
+                {
+                  key: index,
+                  label: (
+                    <h1 className="font-semibold text-primary-color">
+                      {elem?.title}
+                    </h1>
+                  ),
+                  children: (
+                    <p className="rounded border p-1.5 text-xs font-semibold text-gray-700">
+                      {elem?.desc}
+                    </p>
+                  ),
+                },
+              ]}
+            />
+          );
+        })}
       </div>
     </>
   );
