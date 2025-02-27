79286c9f2be07f283662cae0baf55966f3e99877
karansapra124 <sapra7112@gmail.com>
2025-02-27 12:56:52 +0530

    Steps One Started

---
+++ src/Components/News_&_Blog/StepOne.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Container from "../Helper/Container";
+
+const StepOne = () => {
+  return (
+    <>
+      <Container>
+        <div>
+          <h1>NEET Registration 2025</h1>
+          <p>
+            To fill the NEET application form 2025, candidates are required to
+            click on 'fill application form'.On the new page, click on 'NTA NEET
+            registration 2025 link' which will lead to the instructions page
+            which must be read carefully.
+          </p>
+          <p>
+            During NEET-UG registration process, candidates have to fill the
+            below mentioned details.A password must also be created as per the
+            given specifications which is used for all future login. A NEET
+            application number is generated after registration for NEET NTA
+            2025. This user ID is used to login to NEET form fill up 2025.
+          </p>
+          <div>
+            
+          </div>
+        </div>
+      </Container>
+    </>
+  );
+};
+
+export default StepOne;
