119aa501b11c5573084bb65174da177c4af577ef
karansapra124 <sapra7112@gmail.com>
2025-02-27 11:53:06 +0530

    Eligibility criteria started

--- src/Components/News_&_Blog/Steps.jsx
+++ src/Components/News_&_Blog/Steps.jsx
@@ -1,31 +1,45 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { newsData } from "../../Utils/NewsMockData";
 import { Steps } from "antd";
 import Container from "../Helper/Container";
 const NeetSteps = () => {
   const { stepsData } = newsData;
-  const [curr,setCurr] = useState(2)
+  const [curr, setCurr] = useState(0);
+  useEffect(() => {
+    if (curr === stepsData.length ) return; // Jab last step pe pahunch jaye, interval set hi na ho
+  
+    const currInterval = setInterval(() => {
+      setCurr((prev) => (prev < stepsData.length  ? prev + 1 : prev));
+    }, 1000);
+  
+    return () => clearInterval(currInterval); // Cleanup to prevent memory leaks
+  }, [curr, stepsData.length]); // curr ko dependency me rakha taaki jab update ho, effect fire ho
+  
+  
   return (
     <Container>
       <div>
-        <h1 className="text-xl font-bold text-yellow-600">NEET <span className="text-primary-color font-extrabold">Registration</span> 2025</h1>
+        <h1 className="text-xl font-bold text-yellow-600">
+          NEET{" "}
+          <span className="font-extrabold text-primary-color">
+            Registration
+          </span>{" "}
+          2025
+        </h1>
         <Steps
-        
           type="inline"
           current={2}
           className="custom-stepper"
           items={stepsData?.map((elem, index) => {
             return {
               title: (
-                <div className="border py-2 m-4 rounded border-gray-900 w-52 h-10">
-                  <p className="text-center text-gray-900 text-[0.7rem] font-semibold">
+                <div className="m-4 h-10 w-52 rounded border border-gray-900 py-2">
+                  <p className="text-center text-[0.7rem] font-semibold text-gray-900">
                     {elem}
                   </p>
                 </div>
               ),
-              status:index < curr ? "finish" : "wait",
-              
-              
+              status: index < curr ? "finish" : "wait",
             };
           })}
         />
