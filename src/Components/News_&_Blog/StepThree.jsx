379c91f741066cec6dd5bf1eba93a28446d7c133
karansapra124 <sapra7112@gmail.com>
2025-02-27 16:16:20 +0530

    Added Section Of StepThree

--- src/Components/News_&_Blog/StepThree.jsx
+++ src/Components/News_&_Blog/StepThree.jsx
@@ -1,22 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { newsData } from "../../Utils/NewsMockData";
 import Container from "../Helper/Container";
 import { Collapse } from "antd";
+import Divider from "../Helper/Divider";
 const StepThree = () => {
   const { stepThree } = newsData;
+
   return (
     <Container>
-      <div>
-        <h1>
-          Uploading Of Scanned Documents & Images For NEET 2025 Registration
-          Form
+      <div className="flex flex-col gap-2">
+        <h1 className="text-xl font-bold text-yellow-600">
+          <span className="font-extrabold text-primary-color">
+            {" "}
+            Uploading Of Scanned Documents & Images
+          </span>{" "}
+          For NEET 2025 Registration Form
         </h1>
-        <p>
+        <p className="text-xs font-semibold text-gray-700">
           After filling in the details in the NEET form 2025, candidates have to
           upload scanned copies of the photograph and signature as specified.
           Candidates need to click on 'upload documents'. The documents to be
           uploaded are detailed below:
         </p>
+        <Divider className={"h-1 w-12 rounded-full bg-yellow-600"} />
         <div>
           <CollapseData data={stepThree} />
         </div>
@@ -26,36 +32,41 @@ const StepThree = () => {
 };
 
 const CollapseData = ({ data }) => {
+  const [activeKey, setActiveKey] = useState(null);
+
   return (
-    <>
-      <div className="flex flex-col gap-2">
-        {data?.map((elem, index) => {
-          return (
-            <>
-              <Collapse
-                defaultActiveKey={[0]}
-                className="mx-auto w-96"
-                accordion
-                items={[
-                  {
-                    label: (
-                      <h1 className="font-semibold text-primary-color">
-                        {elem?.title}
-                      </h1>
-                    ),
-                    children: (
-                      <p className="rounded border p-1.5 text-xs font-semibold text-gray-700">
-                        {elem?.desc}
-                      </p>
-                    ),
-                  },
-                ]}
-              />
-            </>
-          );
-        })}
+    <div className="flex justify-evenly">
+      <img
+        src="https://img.freepik.com/free-vector/isometric-pharmaceutic-laboratory-research-scientists-composition-with-isolated-character-female-with-laptop-microscope-vector-illustration_1284-66452.jpg?ga=GA1.1.715862983.1733989251&semt=ais_hybrid"
+        className="w-96"
+        alt=""
+      />
+      <div className="flex flex-col items-center justify-center gap-2">
+        {data?.map((elem, index) => (
+          <Collapse
+            key={index}
+            className="mx-auto mb-2 w-96"
+            activeKey={activeKey === index ? index : null}
+            onChange={() => setActiveKey(activeKey === index ? null : index)}
+            items={[
+              {
+                key: index,
+                label: (
+                  <h1 className="font-semibold text-primary-color">
+                    {elem.title}
+                  </h1>
+                ),
+                children: (
+                  <p className="rounded border p-1.5 text-xs font-semibold text-gray-700">
+                    {elem.desc}
+                  </p>
+                ),
+              },
+            ]}
+          />
+        ))}
       </div>
-    </>
+    </div>
   );
 };
 
