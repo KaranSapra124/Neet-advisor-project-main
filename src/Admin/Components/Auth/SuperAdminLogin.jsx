a2755a94d128da4a5ebe877460e98933c9f064c9
karansapra124 <sapra7112@gmail.com>
2025-03-01 15:49:07 +0530

    Added superadmin login page

--- src/Admin/Components/Auth/SuperAdminLogin.jsx
+++ src/Admin/Components/Auth/SuperAdminLogin.jsx
@@ -1,43 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import Container from "../../../Components/Helper/Container";
+import Divider from "../../../Components/Helper/Divider";
+import { FaPaperPlane } from "react-icons/fa";
 const SuperAdminLogin = () => {
+  const [email, setEmail] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(email);
+    setEmail("")
+  };
   return (
     <>
       <Container>
-        <div className="mx-auto lg:flex-row  w-fit rounded  flex-col-reverse flex lg:max-w-screen-lg border">
+        <div className="mx-auto flex w-fit flex-col-reverse rounded border lg:max-w-screen-lg lg:flex-row">
           <img
-            className="lg:w-1/2 w-80"
+            className="w-80 lg:w-1/2"
             src="https://img.freepik.com/free-vector/business-decisions-concept-illustration_114360-4393.jpg?ga=GA1.1.715862983.1733989251&semt=ais_hybrid"
             alt=""
             srcset=""
           />
-          <div className="lg:w-1/2 lg:py-4 py-2 flex flex-col items-center justify-center  w-80 border">
-            <h1 className="text-center lg:text-xl text-md my-2  lg:font-bold font-semibold text-primary-color">
-              Admin Login
+          <div className="flex w-80 flex-col items-center justify-center border py-2 lg:w-1/2 lg:py-4">
+            <h1 className="text-md text-center font-semibold text-primary-color lg:text-lg lg:font-extrabold">
+              Super Admin Login
             </h1>
-            <div className="flex gap-4 flex-col justify-center items-center ">
-              <label className="flex text-sm lg:text-lg font-semibold text-primary-color flex-col">
-                Username:
+            <Divider
+              className={"my-2 h-1 w-12 rounded-full bg-primary-color"}
+            />
+            <form onSubmit={handleSubmit} className="flex flex-col items-center justify-center gap-4">
+              <label className="flex flex-col text-sm font-semibold text-primary-color lg:text-lg">
+                Email:
                 <input
-                  type="text"
-                  className="lg:text-md  text-xs font-bold shadow-sm p-2 shadow-gray-400 rounded"
+                  type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  autoFocus
+                  className="lg:text-md rounded p-2 text-xs font-bold shadow-sm shadow-gray-400 outline-none"
                   name=""
-                  placeholder="Enter Your Username..."
+                  placeholder="Enter Your Registered Mail..."
                   id=""
                 />
               </label>
-              <label className="flex text-sm lg:text-lg font-semibold text-primary-color flex-col">
+              {/* <label className="flex flex-col text-sm font-semibold text-primary-color lg:text-lg">
                 Password:
                 <input
                   type="password"
-                  className="text-xs lg:text-md font-bold shadow-sm p-2 shadow-gray-400 rounded"
+                  className="lg:text-md rounded p-2 text-xs font-bold shadow-sm shadow-gray-400"
                   name=""
                   placeholder="Enter Your Username..."
                   id=""
                 />
-              </label>
-              <button className="text-white lg:text-sm text-xs py-2 bg-yellow-600 w-fit px-1.5 rounded   font-semibold ">Get Me In!</button>
-            </div>
+              </label> */}
+              <button
+                type="submit"
+                className="flex w-fit items-center gap-2 rounded bg-yellow-600 px-1.5 py-2 text-xs font-medium text-white shadow shadow-black lg:text-sm"
+              >
+                Get OTP <FaPaperPlane className="text-xs text-white" />
+              </button>
+            </form>
           </div>
         </div>
       </Container>
