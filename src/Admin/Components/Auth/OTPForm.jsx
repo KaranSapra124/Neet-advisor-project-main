281a849caa1de845032541da025e72c2d4936990
karansapra124 <sapra7112@gmail.com>
2025-03-02 13:01:24 +0530

    UseLocation hook integrated in otp form

--- src/Admin/Components/Auth/OTPForm.jsx
+++ src/Admin/Components/Auth/OTPForm.jsx
@@ -1,12 +1,17 @@
 import React, { useEffect, useRef, useState } from "react";
 import Container from "../../../Components/Helper/Container";
+import { useLocation } from "react-router-dom";
 
 const OTPForm = () => {
+  const location = useLocation();
+  const { state } = location;
+  console.log(state);
   const [otp, setOTP] = useState([]);
   const [otpCount, setOtpCount] = useState(6);
   const [currIndex, setCurrIndex] = useState(0);
   const inputRef = useRef([]);
-  const handleChange = (e, index) => { //Change fn for filling OTP
+  const handleChange = (e, index) => {
+    //Change fn for filling OTP
     const { value } = e.target;
     setOTP(() => {
       const newOTP = [...otp];
@@ -15,7 +20,8 @@ const OTPForm = () => {
     });
     value && index < otpCount && inputRef?.current[index + 1]?.focus();
   };
-  const handleKeyDown = (e, index) => {  //Fn for backspace
+  const handleKeyDown = (e, index) => {
+    //Fn for backspace
     if (e.key === "Backspace") {
       const newOTP = [...otp];
       if (index > 0) {
@@ -26,34 +32,34 @@ const OTPForm = () => {
     }
   };
 
-
-
   return (
     <>
       <Container>
         <div className="mx-auto flex w-full max-w-sm flex-col rounded py-4 shadow shadow-black">
           <h1 className="my-2 text-center text-xl font-bold">Fill The OTP</h1>
-          <div className="mx-auto flex items-center justify-evenly">
-            {Array.from({ length: otpCount }).map((elem, index) => {
-              return (
-                <input
-                  key={index}
-                  ref={(el) => (inputRef.current[index] = el)}
-                  type="number"
-                  maxLength={1}
-                  min={0}
-                  className="mx-1 h-7 w-7 rounded border border-black text-center"
-                  placeholder="0"
-                  value={otp[index]}
-                  onChange={(e) => handleChange(e, index)}
-                  onKeyDown={(e) => handleKeyDown(e, index)}
-                />
-              );
-            })}
-          </div>
-          <button className="mx-auto my-2 w-fit rounded-sm bg-yellow-600 p-1 text-white">
-            Submit
-          </button>
+          <form className="mx-auto flex flex-col items-center justify-evenly">
+            <div>
+              {Array.from({ length: otpCount }).map((elem, index) => {
+                return (
+                  <input
+                    key={index}
+                    ref={(el) => (inputRef.current[index] = el)}
+                    type="number"
+                    maxLength={1}
+                    min={0}
+                    className="mx-1 h-7 w-7 rounded border border-black text-center"
+                    placeholder="0"
+                    value={otp[index]}
+                    onChange={(e) => handleChange(e, index)}
+                    onKeyDown={(e) => handleKeyDown(e, index)}
+                  />
+                );
+              })}
+            </div>
+            <button className="mx-auto my-2 w-fit rounded-md bg-yellow-600 p-1 font-medium text-white">
+              Submit
+            </button>
+          </form>
         </div>
       </Container>
     </>
