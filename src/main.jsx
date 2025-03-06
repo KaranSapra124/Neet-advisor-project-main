ccd427c627c478265edee1140ed1fdc74189cacb
karansapra124 <sapra7112@gmail.com>
2025-03-06 13:20:13 +0530

    Implemeneted React Query

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import "animate.css/animate.compat.css";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 
 import App from "./App.jsx";
+const queryClient = new QueryClient();
 
-createRoot(document.getElementById("root")).render(<App />);
+createRoot(document.getElementById("root")).render(
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>,
+);
