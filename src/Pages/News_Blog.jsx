cda9569723ebd9fb51f26e19eaf4f4e8bd813294
karansapra124 <sapra7112@gmail.com>
2025-03-12 10:20:30 +0530

    Added Blogs

--- src/Pages/News_Blog.jsx
+++ src/Pages/News_Blog.jsx
@@ -1,21 +1,8 @@
 import React, { useEffect, useState } from "react";
 import Root from "../Components/Helper/Root";
-import Hero from "../Components/News_&_Blog/Hero";
-import LatestNews from "../Components/News_&_Blog/LatestNews";
-import NewsCategories from "../Components/News_&_Blog/NewsCategories";
 import NewsLetter from "../Components/News_&_Blog/NewsLetter";
 import scrollToTop from "../Utils/ScrollToTop";
-import NewsBanner from "../Components/News_&_Blog/NewsBanner";
-import Blog from "../Components/News_&_Blog/Blog";
-import SocialLinks from "../Components/Services/SocialLinks";
-import MoreNews from "../Components/News_&_Blog/MoreNews";
-import Schedule from "../Components/News_&_Blog/Schedule";
-import EligibilityCriteria from "../Components/News_&_Blog/EligibilityCriteria";
-import NeetSteps from "../Components/News_&_Blog/Steps";
-import StepOne from "../Components/News_&_Blog/StepOne";
-import StepTwo from "../Components/News_&_Blog/StepTwo";
-import StepThree from "../Components/News_&_Blog/StepThree";
-import StepFour from "../Components/News_&_Blog/StepFour";
+
 const News_Blog = () => {
   const [isOpen, setIsOpen] = useState(false);
   useEffect(() => {
@@ -27,20 +14,7 @@ const News_Blog = () => {
     <>
       {isOpen && <NewsLetter isOpen={isOpen} setIsOpen={setIsOpen} />}
       <Root>
-        <Hero />
-        <NewsCategories />
-        <MoreNews />
-        <Schedule />
-        <EligibilityCriteria />
-        <NeetSteps />
-        <StepOne />
-        <StepTwo />
-        <StepThree/>
-        <StepFour/>
-        {/* <LatestNews />
-        <NewsBanner />
-        <Blog />
-        <SocialLinks /> */}
+        <div className="content"></div>
       </Root>
     </>
   );
