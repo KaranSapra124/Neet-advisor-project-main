a1031da60a157931c66e33b0b407cb6a65890206
karansapra124 <sapra7112@gmail.com>
2025-03-13 11:15:11 +0530

    Updated News Component

--- src/Components/News_&_Blog/LatestNews.jsx
+++ src/Components/News_&_Blog/LatestNews.jsx
@@ -17,24 +17,29 @@ const LatestNews = () => {
   });
   return (
     <>
-      <div className="max-h-96  max-w-sm overflow-x-auto border p-2">
-        <h1 className="rounded bg-gray-200 p-1 text-xs font-extrabold text-primary-color lg:text-xl">
-          Latest News
-        </h1>
-        <Divider className={"my-2 h-1 w-12 rounded-full bg-primary-color"} />
-        <div className="rounded bg-gray-200/50 p-2">
+      <div className="max-h-80 my-4 mx-2 max-w-sm overflow-x-auto shadow-lg">
+        <div className="sticky top-0 z-[99] bg-white p-2">
+          <h1 className="rounded bg-gray-200 p-1 text-xs font-extrabold text-primary-color lg:text-xl">
+            Latest News
+          </h1>
+          <Divider className={"my-2 h-1 w-12 rounded-full bg-primary-color"} />
+        </div>
+        <div className="m-2 rounded bg-gray-200/50 p-2">
           {!isLoading ? (
             data?.map((news, index) => {
               return (
                 <>
-              <div className="flex my-2 cursor-pointer">
-              <span className="mx-1 font-bold text-primary-color">{index+1}.)</span>
-                <div
-                  dangerouslySetInnerHTML={{
-                    __html: news?.generatedHTML?.substring(0, 120),
-                  }}
-                ></div>
-              </div>
+                  <div className="my-2 hover:bg-white p-2 rounded  hover:border-l-2 hover:border-b-2 hover:border-yellow-600 transition-all   flex cursor-pointer">
+                    <span className="mx-1  font-bold text-primary-color">
+                      {index + 1}.)
+                    </span>
+                    <div
+                      className="text-xs lg:text-sm line-clamp-2 "
+                      dangerouslySetInnerHTML={{
+                        __html: news?.generatedHTML?.substring(0, 120),
+                      }}
+                    ></div>
+                  </div>
                 </>
               );
             })
