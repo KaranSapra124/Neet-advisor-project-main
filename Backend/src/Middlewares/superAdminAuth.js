e0965167dc3359c823e496bc925b40a5e5c28fab
karansapra124 <sapra7112@gmail.com>
2025-03-15 11:39:27 +0530

    Updated News Component

--- Backend/src/Middlewares/superAdminAuth.js
+++ Backend/src/Middlewares/superAdminAuth.js
@@ -2,13 +2,15 @@ const Jwt = require("jsonwebtoken");
 
 exports.superAdminAuth = (req, res, next) => {
   try {
+
     const { token } = req.cookies;
     if (!token) res.status(401).send({ message: "No Token Provided!" });
     const id = Jwt.decode(token);
     req.user = id;
     next();
   } catch (err) {
-    return res.status(401).send({ message: "Unauthorized: Invalid Token" });
+    console.log(err)
+    res.status(401).send({ message: "Unauthorized: Invalid Token" });
   }
 };
 exports.adminAuth = (req, res, next) => {
