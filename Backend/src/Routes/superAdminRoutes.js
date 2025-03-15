0eb4b5ef804f5ecc4dd4d952e9dce4c721a97848
karansapra124 <sapra7112@gmail.com>
2025-03-15 11:20:13 +0530

    Super Admin Authentication Improved

--- Backend/src/Routes/superAdminRoutes.js
+++ Backend/src/Routes/superAdminRoutes.js
@@ -43,6 +43,7 @@ Router.get("/get-zoom-meetings", getAuthTokenZoom, getZoomMeetings);
 // Services
 Router.post(
   "/add-service",
+  superAdminAuth,
   upload.fields([
     { name: "video", maxCount: 1 },
     { name: "icon", maxCount: 1 },
@@ -53,6 +54,7 @@ Router.get("/get-services", getServices);
 
 Router.post(
   "/edit-service/:id",
+  superAdminAuth,
   upload.fields([
     { name: "video", maxCount: 1 },
     { name: "icon", maxCount: 1 },
@@ -60,30 +62,35 @@ Router.post(
   editService,
 );
 
-Router.post("/delete-service/:id", deleteService);
+Router.post("/delete-service/:id", superAdminAuth, deleteService);
 // Services Ended
 
 // Students Started
-Router.post("/add-student", upload.single("file"), addStudents);
+Router.post("/add-student", superAdminAuth, upload.single("file"), addStudents);
 Router.get("/get-students", getStudents);
-Router.post("/delete-student/:id", deleteStudent);
-Router.post("/edit-student/:id", upload.single("file"), editStudent);
+Router.post("/delete-student/:id", superAdminAuth, deleteStudent);
+Router.post(
+  "/edit-student/:id",
+  superAdminAuth,
+  upload.single("file"),
+  editStudent,
+);
 
 // Admin Started
-Router.post("/add-admin", addAdmin);
-Router.get("/get-admins", getAdmins);
-Router.post("/delete-admin/:id", deleteAdmin);
-Router.post("/edit-admin/:id", editAdmin);
+Router.post("/add-admin", superAdminAuth, addAdmin);
+Router.get("/get-admins", superAdminAuth, getAdmins);
+Router.post("/delete-admin/:id", superAdminAuth, deleteAdmin);
+Router.post("/edit-admin/:id", superAdminAuth, editAdmin);
 
 // News Started
-Router.post("/add-news", upload.single("file"), addNews);
-Router.post("/add-news-admin", addNewsFn);
+Router.post("/add-news", superAdminAuth, upload.single("file"), addNews);
+Router.post("/add-news-admin", superAdminAuth, addNewsFn);
 Router.get("/get-news", getNews);
-Router.post("/edit-news/:id", editNews);
-Router.post("/delete-news/:id", deleteNews);
-Router.get("/get-latest-news", getLatestNews);
-Router.post("/get-related-news", getRelatedNews);
-Router.get("/get-news/:id", getSingleNews);
+Router.post("/edit-news/:id", superAdminAuth, editNews);
+Router.post("/delete-news/:id", superAdminAuth, deleteNews);
+Router.get("/get-latest-news", superAdminAuth, getLatestNews);
+Router.post("/get-related-news", superAdminAuth, getRelatedNews);
+Router.get("/get-news/:id", superAdminAuth, getSingleNews);
 // News Completed
 
 module.exports = Router;
