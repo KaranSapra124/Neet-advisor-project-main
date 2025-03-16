6d2f0d4cdb0c401d2018b5391ff06f0b772ba64e
karansapra124 <sapra7112@gmail.com>
2025-03-16 15:54:49 +0530

    Seminar integration on frontend started

--- src/Components/PG_Seminar/Hero.jsx
+++ src/Components/PG_Seminar/Hero.jsx
@@ -5,28 +5,33 @@ import "./Pg_Seminar.css";
 import { FaGraduationCap } from "react-icons/fa6";
 import { FaArrowRight, FaShieldAlt } from "react-icons/fa";
 import ScrollAnimation from "react-animate-on-scroll";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+const fetchSeminars = async () => {
+  const { data } = await axios.get(
+    `${import.meta.env.VITE_BACKEND_URL}super-admin/get-seminars`,
+  );
+  console.log(data);
+  return data?.allSeminars;
+};
 const Hero = () => {
-  const seminarData = [
-    {
-      location: "Mumbai",
-      date: "28 January 2025",
-      venue: "Grand Convention Center, Mumbai",
-      video:
-        "https://videos.pexels.com/video-files/7424129/7424129-sd_640_360_30fps.mp4",
-      isAvailable: true,
-    },
-    {
-      location: "Delhi",
-      date: "10 February 2025",
-      venue: "Delhi International Auditorium",
-      video:
-        "https://videos.pexels.com/video-files/7424129/7424129-sd_640_360_30fps.mp4",
-      isAvailable: false,
-    },
-  ];
+  const { data } = useQuery({
+    queryKey: ["allSeminars"],
+    queryFn: fetchSeminars,
+  });
+
+  const formatDate = (dateString) => {
+    const [yy, mm, dd] = dateString.split("-").map(Number);
+    return new Date(yy, mm - 1, dd).toLocaleDateString("en-GB", {
+      day: "2-digit",
+      month: "long",
+      year: "numeric",
+    });
+  };
+
   return (
     <>
-      <Container className={"relative lg:h-screen h-full"}>
+      <Container className={"relative h-full lg:h-screen"}>
         <video
           src="./Seminar/Seminar-video.mp4"
           autoPlay
@@ -34,7 +39,7 @@ const Hero = () => {
           muted
           className="absolute inset-0 h-full w-full object-cover"
         ></video>
-        <div className="absolute inset-0 h-full w-full bg-gradient-to-r lg:from-black/80 from-black/60 lg:via-black/80 via-black/80 lg:to-gray-900/40 to-gray-900/50"></div>
+        <div className="absolute inset-0 h-full w-full bg-gradient-to-r from-black/60 via-black/80 to-gray-900/50 lg:from-black/80 lg:via-black/80 lg:to-gray-900/40"></div>
         <div className="relative flex h-full w-full flex-col items-center justify-between">
           <ScrollAnimation
             animateIn="backInDown"
@@ -42,29 +47,29 @@ const Hero = () => {
             animateOnce={true}
             delay={1}
           >
-            <div className="flex flex-col justify-center items-center gap-4">
-              <div className="w-fit rounded-l-full rounded-r-full  bg-yellow-600 px-2 py-1 lg:text-sm text-[0.6rem] font-bold uppercase text-white">
+            <div className="flex flex-col items-center justify-center gap-4">
+              <div className="w-fit rounded-l-full rounded-r-full bg-yellow-600 px-2 py-1 text-[0.6rem] font-bold uppercase text-white lg:text-sm">
                 # 1 Neet counsellor
               </div>
-              <h1 className="lg:text-4xl text-sm  text-center font-extrabold lg:leading-tight leading-5 text-yellow-600">
+              <h1 className="text-center text-sm font-extrabold leading-5 text-yellow-600 lg:text-4xl lg:leading-tight">
                 Fuel Your Dream <br /> of Becoming a Doctor <br />
-                <span className="lg:text-6xl  text-3xl  text-center font-extrabold text-primary-color brightness-200">
+                <span className="text-center text-3xl font-extrabold text-primary-color brightness-200 lg:text-6xl">
                   NEET PG 2025
                 </span>
               </h1>
-              <p className="lg:max-w-md max-w-52 text-center lg:text-xs text-[0.5rem] font-extrabold text-gray-300">
+              <p className="max-w-52 text-center text-[0.5rem] font-extrabold text-gray-300 lg:max-w-md lg:text-xs">
                 Prepare yourself for the NEET UG exam with expert guidance and
                 tips. Join us for insightful sessions, mock tests, and career
                 counseling.
               </p>
-             <div className="flex flex-row  gap-4 justify-center my-2 w-full">
-             <button className="lg:text-sm text-xs w-fit rounded-l-lg rounded-r-lg bg-yellow-600 px-3 py-2 font-bold text-white">
-                Book Now!
-              </button>
-              <button className="lg:text-sm text-xs w-fit rounded-l-lg rounded-r-lg bg-black/50 shadow shadow-white px-3 py-2 font-bold text-white">
-                Learn more!
-              </button>
-             </div>
+              <div className="my-2 flex w-full flex-row justify-center gap-4">
+                <button className="w-fit rounded-l-lg rounded-r-lg bg-yellow-600 px-3 py-2 text-xs font-bold text-white lg:text-sm">
+                  Book Now!
+                </button>
+                <button className="w-fit rounded-l-lg rounded-r-lg bg-black/50 px-3 py-2 text-xs font-bold text-white shadow shadow-white lg:text-sm">
+                  Learn more!
+                </button>
+              </div>
             </div>
           </ScrollAnimation>
           <ScrollAnimation
@@ -74,27 +79,32 @@ const Hero = () => {
             animateOnce={true}
             delay={1}
           >
-            <div className="flex lg:flex-row flex-col h-fit ">
-              {seminarData?.map((seminar, index) => {
+            <div className="flex h-fit flex-col lg:flex-row">
+              {data?.map((seminar, index) => {
                 return (
-                  <div className="cursor-pointer relative shadow mx-4 lg:my-0 my-4  border bg-white/10 border-white rounded-lg lg:p-3.5 p-1.5 transition-all duration-100 hover:scale-105">
-                    <img src="./Webinar/calendar-bg-removed.gif" className="w-7 h-7 absolute -left-3 shadow-md shadow-yellow-600 bg-white rounded-full -top-5 " alt="" srcset="" />
+                  <div className="relative mx-4 my-4 cursor-pointer rounded-lg border border-white bg-white/10 p-1.5 shadow transition-all duration-100 hover:scale-105 lg:my-0 lg:p-3.5">
+                    <img
+                      src="./Webinar/calendar-bg-removed.gif"
+                      className="absolute -left-3 -top-5 h-7 w-7 rounded-full bg-white shadow-md shadow-yellow-600"
+                      alt=""
+                      srcset=""
+                    />
                     <div className="w-fit rounded-l-full rounded-r-full bg-yellow-600 px-2 py-1 text-xs font-bold uppercase text-yellow-200">
-                      # {seminar?.location}
+                      # {seminar?.state}
                     </div>
-                    <div className="flex items-center lg:gap-4 gap-2 justify-between  p-2">
-                      <h1 className="lg:text-3xl text-xs font-bold text-white">
-                        {seminar?.date}
+                    <div className="flex items-center justify-between gap-2 p-2 lg:gap-4">
+                      <h1 className="text-xs font-bold text-white lg:text-3xl">
+                        {formatDate(seminar?.date)}
                       </h1>
                       <div
-                        className={`w-fit rounded-md ${seminar?.isAvailable ? "border-2 border-primary-color bg-white/50 text-primary-color" : "border-2 border-gray-800 bg-white/50 text-gray-800"} px-1.5 lg:px-2 py-1 lg:text-sm text-[0.5rem] font-bold uppercase`}
+                        className={`w-fit rounded-md ${seminar?.isAvailable ? "border-2 border-primary-color bg-white/50 text-primary-color" : "border-2 border-gray-800 bg-white/50 text-gray-800"} px-1.5 py-1 text-[0.5rem] font-bold uppercase lg:px-2 lg:text-sm`}
                       >
-                        {seminar?.isAvailable ? "Available" : "Not Available"}
+                        Available
                       </div>
-                      <FaArrowRight className="animate-scaleUp text-yellow-600 lg:text-sm text-xs" />
+                      <FaArrowRight className="animate-scaleUp text-xs text-yellow-600 lg:text-sm" />
                     </div>
-                    <div className="text-xs mx-2 font-semibold text-gray-200">
-                      {seminar.venue}
+                    <div className="mx-2 text-xs font-semibold text-gray-200">
+                      {seminar.location}
                     </div>
                     {/* {index !== seminarData?.length - 1 && (
                       <Divider className="my-5 rotate-90 h-1 w-full rounded-full bg-yellow-600" />
