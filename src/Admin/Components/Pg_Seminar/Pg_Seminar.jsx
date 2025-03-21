023ede81c681f0505817cccd5451ae83a7c10844
karansapra124 <sapra7112@gmail.com>
2025-03-21 14:42:28 +0530

    Added middleware to routes

--- src/Admin/Components/Pg_Seminar/Pg_Seminar.jsx
+++ src/Admin/Components/Pg_Seminar/Pg_Seminar.jsx
@@ -22,7 +22,9 @@ import PG_seminar from "../../../Pages/PG_seminar";
 const fetchSeminar = async () => {
   try {
     const res = await axios.get(
-      `${import.meta.env.VITE_BACKEND_URL}super-admin/get-seminars`,
+      `${import.meta.env.VITE_BACKEND_URL}super-admin/get-seminars`,null,{
+        withCredentials:true
+      }
     );
     const { data } = res;
     return data?.allSeminars;
