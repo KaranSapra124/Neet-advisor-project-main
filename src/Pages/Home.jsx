0d0904a29c7923d6e8f9cf136e60e753bf00f07c
karansapra124 <sapra7112@gmail.com>
2025-03-22 14:42:23 +0530

    Queries Started

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,41 +1,53 @@
-import React, { useEffect } from 'react'
-import Hero from '../Components/Home/Hero'
-import CounsellingSection from '../Components/Home/CounsellingSection'
-import WhyUsSection from '../Components/Home/WhyUsSection'
-import NewsSection from '../Components/Home/NewsSection'
-import Stats from '../Components/Home/Stats'
-import Testimonials from '../Components/Home/Testimonials'
-import ContactBanner from '../Components/Home/ContactBanner'
-import Footer from '../Components/Home/Footer'
-import Navbar from '../Components/Home/Navbar'
-import UpperNavbar from '../Components/Helper/UpperNavbar'
-import Gallery from '../Components/Home/Gallery'
-import Root from '../Components/Helper/Root'
-import scrollToTop from '../Utils/ScrollToTop'
+import React, { useEffect, useState } from "react";
+import Hero from "../Components/Home/Hero";
+import CounsellingSection from "../Components/Home/CounsellingSection";
+import WhyUsSection from "../Components/Home/WhyUsSection";
+import NewsSection from "../Components/Home/NewsSection";
+import Stats from "../Components/Home/Stats";
+import Testimonials from "../Components/Home/Testimonials";
+import ContactBanner from "../Components/Home/ContactBanner";
+import Footer from "../Components/Home/Footer";
+import Navbar from "../Components/Home/Navbar";
+import UpperNavbar from "../Components/Helper/UpperNavbar";
+import Gallery from "../Components/Home/Gallery";
+import Root from "../Components/Helper/Root";
+import scrollToTop from "../Utils/ScrollToTop";
+import ContactModal from "../Utils/ContactModal";
 // import ImageSection from '../Components/Home/ImageSection'
 
 const Home = () => {
-    useEffect(()=>{
-        scrollToTop()
-    },[])
-    return (
-        <>
-            {/* <UpperNavbar /> */}
-            {/* <Navbar /> */}
-            <Root>
-                <Hero />
-                {/* <ImageSection /> */}
-                <CounsellingSection />
-                <WhyUsSection />
-                <NewsSection />
-                <Stats />
-                <Testimonials />
-                <Gallery />
-                <ContactBanner />
-            </Root>
-            {/* <Footer /> */}
-        </>
-    )
-}
+  const [isOpen, setIsOpen] = useState(false);
 
-export default Home
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setIsOpen(true);
+    }, 3500);
+    return () => clearTimeout(timeout);
+  }, []);
+  useEffect(() => {
+    scrollToTop();
+  }, []);
+  return (
+    <>
+      {/* <UpperNavbar /> */}
+      {/* <Navbar /> */}
+      {isOpen && (
+        <ContactModal open={isOpen} setIsOpen={() => setIsOpen(false)} />
+      )}
+      <Root>
+        <Hero />
+        {/* <ImageSection /> */}
+        <CounsellingSection />
+        <WhyUsSection />
+        <NewsSection />
+        <Stats />
+        <Testimonials />
+        <Gallery />
+        <ContactBanner />
+      </Root>
+      {/* <Footer /> */}
+    </>
+  );
+};
+
+export default Home;
