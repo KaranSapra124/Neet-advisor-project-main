678efc2208f7d7293e1718051c3343487e28b29a
karansapra124 <sapra7112@gmail.com>
2025-03-22 14:23:46 +0530

    Queries Started

--- Backend/src/Models/Queries.js
+++ Backend/src/Models/Queries.js
@@ -6,4 +6,26 @@ const query = new Schema({
     type: String,
     required: true,
   },
+  PhoneNumber: {
+    type: String,
+    required: true,
+  },
+  Email: {
+    type: String,
+    required: true,
+  },
+  ExamType: {
+    type: String,
+    required: true,
+    enum: ["UG", "PG"],
+  },
+  Option: {
+    type: String,
+    requuired: true,
+  },
+  Description: {
+    type: String,
+  },
 });
+
+module.exports = mongoose.model("Query", query);
