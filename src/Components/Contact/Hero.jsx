0a72425f27fc2fd819ec44fe208b4499a831584c
karansapra124 <sapra7112@gmail.com>
2025-03-23 10:39:41 +0530

    Adding Yesterday Changes

--- src/Components/Contact/Hero.jsx
+++ src/Components/Contact/Hero.jsx
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from "react";
-import { Switch, Dropdown } from "antd";
+import { Switch, Dropdown, Input } from "antd";
 import Container from "../Helper/Container";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
+import { toast } from "react-toastify";
+
 const fetchSeminars = async () => {
   const { data } = await axios.get(
     `${import.meta.env.VITE_BACKEND_URL}super-admin/get-seminars`,
@@ -51,39 +53,81 @@ const Hero = () => {
   );
 };
 
-const FormComponent = ({ requestedState , checkState }) => {
- 
-  // <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3769.836943225087!2d72.8611785!3d19.1148079!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7c827160d6221%3A0x2d16cae2ae28a379!2sNEET%20ADVISOR!5e0!3m2!1sen!2sin!4v1742364176493!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-  const [isUG, setIsUg] = useState(true);
-  const [formData, setFormData] = useState({});
+const statesList = [
+  "Andhra Pradesh",
+  "Arunachal Pradesh",
+  "Assam",
+  "Bihar",
+  "Chhattisgarh",
+  "Goa",
+  "Gujarat",
+  "Haryana",
+  "Himachal Pradesh",
+  "Jharkhand",
+  "Karnataka",
+  "Kerala",
+  "Madhya Pradesh",
+  "Maharashtra",
+  "Manipur",
+  "Meghalaya",
+  "Mizoram",
+  "Nagaland",
+  "Odisha",
+  "Punjab",
+  "Rajasthan",
+  "Sikkim",
+  "Tamil Nadu",
+  "Telangana",
+  "Tripura",
+  "Uttar Pradesh",
+  "Uttarakhand",
+  "West Bengal",
+];
+
+const FormComponent = ({ requestedState, checkState }) => {
+  const [formData, setFormData] = useState({
+    Name: "",
+    PhoneNumber: "",
+    Email: "",
+    State: "",
+    Description: "",
+    captcha: false,
+    termsAccepted: false,
+  });
 
   const handleChange = (e) => {
-    const { name, value } = e.target;
+    const { name, value, type, checked } = e.target;
     setFormData((prev) => ({
       ...prev,
-      [name]: value,
+      [name]: type === "checkbox" ? checked : value,
     }));
   };
 
-  const courseOptions = [
-    {
-      key: 1,
-      label: "General Query",
-      value: "GeneralQuery",
-    },
-    {
-      key: 2,
-      label: "Escalation",
-      value: "Escalation",
-    },
-  ];
-
-  useEffect(() => {
-    setFormData((prev) => ({
-      ...prev,
-      exam: isUG ? "Ug" : "Pg",
-    }));
-  }, [isUG]);
+  const handleSubmit = async () => {
+    if (formData.captcha === false) {
+      alert("Please verify the Captcha!");
+    }
+    try {
+      const { data } = await axios.post(
+        `${import.meta.env.VITE_BACKEND_URL}super-admin/add-query`,
+        formData,
+        { withCredentials: true },
+      );
+      toast.success(data?.message);
+      setFormData({
+        Name: "",
+        PhoneNumber: "",
+        Email: "",
+        State: "",
+        Description: "",
+        captcha: false,
+        termsAccepted: false,
+      });
+    } catch (err) {
+      toast.error(err.response.data.message);
+    }
+    // setIsOpen(false);
+  };
 
   return (
     <div className={"flex flex-col lg:flex-row"}>
@@ -91,8 +135,7 @@ const FormComponent = ({ requestedState , checkState }) => {
         <img
           src="call.gif"
           className="absolute -left-5 -top-5 h-8 w-8 rounded-full shadow-md shadow-yellow-600 lg:h-10 lg:w-10"
-          alt=""
-          srcset=""
+          alt="call icon"
         />
 
         <div className="space-y-4">
@@ -102,73 +145,72 @@ const FormComponent = ({ requestedState , checkState }) => {
               type="text"
               name="Name"
               onChange={handleChange}
-              className="w-full rounded-lg bg-white px-4 py-2 text-xs text-gray-500 placeholder-gray-400 shadow-md focus:outline-none focus:ring-2 focus:ring-yellow-600 lg:text-sm"
+              className="w-full rounded-lg bg-white px-4 py-2 text-xs text-gray-500 shadow-md focus:outline-none focus:ring-2 focus:ring-yellow-600 lg:text-sm"
             />
             <input
               placeholder="Enter Your Number..."
               type="number"
-              min={0}
-              name="phoneNumber"
+              name="PhoneNumber"
               onChange={handleChange}
-              className="w-full rounded-lg bg-white px-4 py-2 text-xs text-gray-500 placeholder-gray-400 shadow-md focus:outline-none focus:ring-2 focus:ring-yellow-600 lg:text-sm"
+              className="w-full rounded-lg bg-white px-4 py-2 text-xs text-gray-500 shadow-md focus:outline-none focus:ring-2 focus:ring-yellow-600 lg:text-sm"
             />
             <input
               placeholder="Enter Your Email..."
               type="email"
-              name="email"
+              name="Email"
               onChange={handleChange}
-              className="w-full rounded-lg bg-white px-4 py-2 text-xs text-gray-500 placeholder-gray-400 shadow-md focus:outline-none focus:ring-2 focus:ring-yellow-600 md:col-span-2 lg:text-sm"
+              className="w-full rounded-lg bg-white px-4 py-2 text-xs text-gray-500 shadow-md focus:outline-none focus:ring-2 focus:ring-yellow-600 md:col-span-2 lg:text-sm"
             />
           </div>
 
-          <div className="space-y-4">
-            <div className="flex items-center justify-between">
-              <Switch
-                checkedChildren="UG"
-                unCheckedChildren="PG"
-                defaultChecked
-                checked={isUG}
-                onChange={(checked) => setIsUg(checked)}
-                className="bg-yellow-600 !text-xs lg:!text-sm"
-              />
+          <select
+            name="State"
+            onChange={handleChange}
+            className="w-full rounded-lg bg-white px-4 py-2 text-xs text-gray-500 shadow-md focus:outline-none focus:ring-2 focus:ring-yellow-600 lg:text-sm"
+          >
+            <option value="">Select Your State</option>
+            {statesList.map((state) => (
+              <option key={state} value={state}>
+                {state}
+              </option>
+            ))}
+          </select>
 
-              <Dropdown
-                menu={{
-                  items: courseOptions.map((option) => ({
-                    key: option.key,
-                    label: (
-                      <div
-                        className="text-xs lg:text-sm"
-                        onClick={() =>
-                          setFormData((prev) => ({
-                            ...prev,
-                            courseDetail: option.value,
-                          }))
-                        }
-                      >
-                        {option.label}
-                      </div>
-                    ),
-                  })),
-                }}
-              >
-                <button className="rounded-lg bg-white px-4 py-2 text-xs text-gray-400 shadow-md transition-colors hover:bg-gray-300 lg:text-sm">
-                  Select Option
-                </button>
-              </Dropdown>
-            </div>
+          <textarea
+            name="Description"
+            placeholder="Description (Optional)"
+            rows={3}
+            onChange={handleChange}
+            className="w-full resize-none rounded-lg bg-white px-4 py-2 text-xs text-gray-500 shadow-md focus:outline-none focus:ring-2 focus:ring-yellow-600 lg:text-sm"
+          />
 
-            <textarea
-              name="description"
-              placeholder="Description (Optional)"
-              rows={3}
+          <div className="flex items-center space-x-2">
+            <input
+              type="checkbox"
+              name="termsAccepted"
+              checked={formData.termsAccepted}
               onChange={handleChange}
-              className="w-full resize-none rounded-lg bg-white px-4 py-2 text-xs text-gray-500 placeholder-gray-400 shadow-md focus:outline-none focus:ring-2 focus:ring-yellow-600 lg:text-sm"
+              className="h-4 w-4 rounded border-gray-300 focus:ring-yellow-600"
             />
+            <label className="text-xs text-gray-600 lg:text-sm">
+              I agree to receive promotional & informational communication from
+              <span className="font-semibold text-yellow-600">
+                {" "}
+                NEET ADVISOR
+              </span>
+              through emails, calls, or SMS messages.*
+            </label>
+          </div>
+          <div className="float-left flex">
+            <Input type="checkbox" name="captcha" onChange={handleChange} />
+            <p className="mx-2 text-xs font-semibold text-gray-800">
+              ReCAPTCHA
+            </p>
+            {/* <ReCAPTCHA sitekey="YOUR_SITE_KEY" onChange={(value) => setForm({ ...form, captcha: value })} /> */}
           </div>
 
           <button
-            onClick={() => console.log(formData)}
+            onClick={handleSubmit}
             className="w-full transform rounded-lg bg-yellow-600 py-2 text-xs font-extrabold text-white transition-colors hover:scale-[1.02] hover:bg-yellow-500 focus:outline-none focus:ring-2 focus:ring-yellow-500 lg:py-3 lg:text-sm lg:font-bold"
           >
             Send
@@ -191,7 +233,7 @@ const FormComponent = ({ requestedState , checkState }) => {
           allowFullScreen=""
           loading="lazy"
           referrerPolicy="no-referrer-when-downgrade"
-        ></iframe>
+        />
       )}
     </div>
   );
