34fcb5b4360dae03a68543c754222c5dd0c8a5a2
karansapra124 <sapra7112@gmail.com>
2025-04-01 15:41:14 +0530

    Added Contact Modal To Enquiry Now Button

--- src/Components/Helper/Container.jsx
+++ src/Components/Helper/Container.jsx
@@ -1,16 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import ContactModal from "../../Utils/ContactModal";
 
 const Container = ({ children, className }) => {
+  const [open, setOpen] = useState(false);
   return (
     <>
+      {open && <ContactModal open={open} setIsOpen={() => setOpen(false)} />}
       <div className={`${className} relative mx-auto p-10 lg:p-12`}>
         {/* Button with absolute positioning and rotation */}
 
         {children}
       </div>
       <button
-        className="fixed -right-10 top-1/2 -translate-y-1/2 rotate-90 transform rounded bg-primary-color p-1 text-lg font-bold text-white"
-        style={{ zIndex: 10 }}
+        onClick={() => setOpen(true)}
+        className="fixed -right-10 top-1/2 z-10 -translate-y-1/2 rotate-90 transform rounded bg-primary-color p-1 text-lg font-bold text-white"
       >
         Enquire Now
       </button>
