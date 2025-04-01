8f92e40d4f3bf94ee9145a9f0c4d83c55a9a8236
karansapra124 <sapra7112@gmail.com>
2025-04-01 11:01:20 +0530

    Changed Book a session to Free One to One Session In Homepage Hero section

--- src/Components/Home/Hero.jsx
+++ src/Components/Home/Hero.jsx
@@ -9,50 +9,54 @@ import ImageContainer from "../Home/ImageSection";
 
 const Hero = () => {
   return (
-    <div className="px-7  py-8  relative z-10 flex items-center justify-center lg:flex-row flex-col lg:px-5 lg:py-10">
-      <div className="flex w-full flex-col  items-center">
-        <div className="flex  items-center lg:justify-center justify-around w-full">
-          <h1 className="primary lg:text-3xl text-lg font-bold ">
+    <div className="relative z-10 flex flex-col items-center justify-center px-7 py-8 lg:flex-row lg:px-5 lg:py-10">
+      <div className="flex w-full flex-col items-center">
+        <div className="flex w-full items-center justify-around lg:justify-center">
+          <h1 className="primary text-lg font-bold lg:text-3xl">
             Complete Handholding for <br />{" "}
             {/* <span className="font-bold text-yellow-600"> */}
-              <span className="font-extrabold text-yellow-600">NEET Counselling Guidance</span> 
+            <span className="font-extrabold text-yellow-600">
+              NEET Counselling Guidance
+            </span>
             {/* </span> */}
           </h1>
           <Lottie
-            className="z-0 lg:w-[8rem] w-[4rem] "
+            className="z-0 w-[4rem] lg:w-[8rem]"
             animationData={medicalReception}
             loop={true}
           />
         </div>
-        <div className=" text-gray-700">
-          <p className="text-wrap lg:text-sm text-[0.5rem] ">
-            <span className="lg:font-extrabold font-bold "> 23,33,863</span> students in India
-            aspire to study MBBS and BDS & AYUSH.Only 8.3% could achieve it in
-            2024.
+        <div className="text-gray-700">
+          <p className="text-wrap text-[0.5rem] lg:text-sm">
+            <span className="font-bold lg:font-extrabold"> 23,33,863</span>{" "}
+            students in India aspire to study MBBS and BDS & AYUSH.Only 8.3%
+            could achieve it in 2024.
           </p>
-          <p className="py-2 lg:text-sm text-[0.5rem]">
+          <p className="py-2 text-[0.5rem] lg:text-sm">
             Complete <span className="font-extrabold">NEET</span> Counselling
             Guidance till Admission. NEET UG & NEET PG Counselling Experts Since
             2016.
           </p>
         </div>
-        <button className="lg:mt-5 mt-4 lg:text-sm text-xs rounded-lg bg-yellow-600 px-4 py-2 font-bold text-white transition-all duration-300 hover:bg-yellow-500">
-          Book a Free session
+        <button className="mt-4 rounded-lg bg-yellow-600 px-4 py-2 text-xs font-bold text-white transition-all duration-300 hover:bg-yellow-500 lg:mt-5 lg:text-sm">
+          Free One To One Session
         </button>
-        <div className="flex  lg:py-5 py-8">
+        <div className="flex py-8 lg:py-5">
           <div className="gabriela-regular mr-10">
-            <h1 className="mb-2 lg:text-2xl text-base font-bold text-indigo-950">50K+</h1>
+            <h1 className="mb-2 text-base font-bold text-indigo-950 lg:text-2xl">
+              50K+
+            </h1>
             <p className="lg:text-md text-xs font-semibold text-gray-700">
               Sessions Completed
             </p>
           </div>
           <div className="flex items-center">
-            <FcGoogle className="mr-3  lg:text-4xl text-xl" />
+            <FcGoogle className="mr-3 text-xl lg:text-4xl" />
             <div className="flex flex-col">
               <div className="mb-2 flex">
-                <Rate className="lg:text-lg text-sm " count={5} value={5} />
+                <Rate className="text-sm lg:text-lg" count={5} value={5} />
               </div>
-              <p className="lg:text-md text-[0.7rem]  font-bold text-gray-700">
+              <p className="lg:text-md text-[0.7rem] font-bold text-gray-700">
                 5 / 5 | 4406 Reviews
               </p>
             </div>
