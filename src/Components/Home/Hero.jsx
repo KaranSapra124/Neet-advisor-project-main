d17713395c00903a95bff2aacb440100c15ad222
karansapra124 <sapra7112@gmail.com>
2025-04-02 16:41:33 +0530

    Improved Hero Section In HomePage

--- src/Components/Home/Hero.jsx
+++ src/Components/Home/Hero.jsx
@@ -1,16 +1,20 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FcGoogle } from "react-icons/fc";
 import { Rate } from "antd";
-import Container from "../Helper/Container";
 import Lottie from "lottie-react";
-
 import medicalReception from "./Medical Clipboard.json";
 import ImageContainer from "../Home/ImageSection";
-import QuickLinks from "../Helper/QuickLinks";
 import BookSessionModal from "../../Utils/BookSessionModal";
+import ScrollAnimation from "react-animate-on-scroll";
 
 const Hero = () => {
   const [open, setOpen] = useState(false);
+  useEffect(() => {
+    const videoId = document.getElementById("heroVideo");
+    window.addEventListener("load", () => {
+      videoId.play();
+    });
+  }, []);
   return (
     <>
       {open && (
@@ -20,76 +24,55 @@ const Hero = () => {
           key={open}
         />
       )}
-      <div className="flex  justify-center">
-        {/* <QuickLinks /> */}
-        <div className="relative z-10 flex max-w-screen-xl flex-col lg:gap-5 gap-2 items-center justify-center px-7 py-8 lg:px-5 lg:py-10">
-          <div className="flex lg:gap-3 gap-2 w-full flex-col items-center">
-            <div className="flex  w-full items-center justify-around lg:justify-center">
-              <div className="flex justify-center flex-col items-center lg:gap-2 gap-1">
-                <h1 className="text-center font-extrabold text-yellow-600 lg:text-5xl">
-                  NEET ADVISOR{" "}
-                </h1>{" "}
-                <h1 className="primary text-lg font-bold lg:text-[2.2rem]">
-                  Pioneer Of Smart Counseling services for NEET Aspirants <br />{" "}
-                  {/* <span className="font-bold text-yellow-600"> */}
-                  {/* </span> */}
+      <ScrollAnimation animateIn="fadeIn" duration={1.5} animateOnce={true} initiallyVisible>
+        <div className="relative flex justify-center">
+          <video
+            id="heroVideo"
+            className="w-full"
+            autoPlay
+            loop
+            muted
+            src="18+Golden.mp4"
+          ></video>
+          {/* Content placed over the video */}
+          <div className="absolute right-5 top-1/4 flex max-w-screen-lg flex-col items-center gap-2 p-2 lg:right-10">
+            <div className="flex w-full items-center justify-center">
+              <div className="flex flex-col items-center gap-1 lg:gap-2">
+                <h1 className="text-center font-extrabold text-yellow-600 lg:text-6xl">
+                  NEET ADVISOR
+                </h1>
+                <h1 className="primary text-center text-lg font-bold leading-tight lg:text-[2rem]">
+                  Pioneer Of Smart Counseling services for NEET Aspirants
                 </h1>
               </div>
               <Lottie
-                className="z-0 w-[4rem] lg:w-[6rem]"
+                className="relative right-2 top-6 z-0 w-[4rem] lg:w-[6rem]"
                 animationData={medicalReception}
                 loop={true}
               />
             </div>
-            <div className="max-w-screen-lg text-center text-gray-700 flex flex-col lg:gap-2 gap-1">
-              <p className="text-wrap text-[0.5rem]  lg:text-[0.9rem]">
+            <div className="flex max-w-screen-lg flex-col gap-1 text-center text-gray-800 lg:gap-2">
+              <p className="text-wrap text-[0.5rem] lg:text-[1.05rem]">
                 <span className="font-bold lg:font-extrabold">
                   {" "}
                   NEET Advisor
                 </span>{" "}
                 is a team of highly skilled professionals dedicated to providing
                 comprehensive guidance on{" "}
-                <span className="font-bold lg:font-extrabold"> NEET</span>{" "}
+                <span className="font-bold lg:font-extrabold"> NEET </span>
                 counselling.
               </p>
-              <p className="text-[0.5rem] lg:text-[0.9rem]">
+              <p className="mx-auto max-w-4xl text-center text-[0.5rem] lg:text-[1.05rem]">
                 Our <span className="font-extrabold">Expertise</span> ensures
-                seamless support throughout the counselling process,application
-                process,documentation and admission procedures for medical
+                seamless support throughout the counselling process, application
+                process, documentation, and admission procedures for medical
                 colleges across India and internationally.
               </p>
             </div>
-            {/* <button
-              onClick={() => setOpen(true)}
-              className="mt-4 rounded-lg bg-yellow-600 px-4 py-2 text-xs font-bold text-white transition-all duration-300 hover:bg-yellow-500 lg:mt-5 lg:text-sm"
-            >
-              Free One To One Session
-            </button>
-            <div className="flex py-8 lg:py-5">
-              <div className="gabriela-regular mr-10">
-                <h1 className="mb-2 text-base font-bold text-indigo-950 lg:text-2xl">
-                  50K+
-                </h1>
-                <p className="lg:text-md text-xs font-semibold text-gray-700">
-                  Sessions Completed
-                </p>
-              </div>
-              <div className="flex items-center">
-                <FcGoogle className="mr-3 text-xl lg:text-4xl" />
-                <div className="flex flex-col">
-                  <div className="mb-2 flex">
-                    <Rate className="text-sm lg:text-lg" count={5} value={5} />
-                  </div>
-                  <p className="lg:text-md text-[0.7rem] font-bold text-gray-700">
-                    5 / 5 | 4406 Reviews
-                  </p>
-                </div>
-              </div>
-            </div> */}
           </div>
-          <ImageContainer />
         </div>
-      </div>
+      </ScrollAnimation>
+      <ImageContainer />
     </>
   );
 };
